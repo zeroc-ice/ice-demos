59116bb0b5ddf5a90f42ab36f1e1550ed48e4eaf
Bernard Normier <bernard@zeroc.com>
2016-05-31 14:37:21 -0400

    Updated version to 3.7a2

--- csharp/Ice/wpf/Properties/AssemblyInfo.cs
+++ csharp/Ice/wpf/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.51")]
-[assembly: AssemblyFileVersion("3.7.51")]
+[assembly: AssemblyVersion("3.7.52")]
+[assembly: AssemblyFileVersion("3.7.52")]
