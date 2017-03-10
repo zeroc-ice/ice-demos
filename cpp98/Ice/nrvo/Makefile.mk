c531e8321d0d111755f97bb3e3db5f80402f8123
Bernard Normier <bernard@zeroc.com>
2017-03-10 23:16:27 +0000

    Fixed copyright notice of makefiles

--- cpp98/Ice/nrvo/Makefile.mk
+++ cpp98/Ice/nrvo/Makefile.mk
@@ -2,9 +2,6 @@
 #
 # Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
 #
-# This copy of Ice is licensed to you under the terms described in the
-# ICE_LICENSE file included in this distribution.
-#
 # **********************************************************************
 
 $(demo)_server_sources	= Server.cpp Nrvo.ice MyStringSeq.cpp NrvoI.cpp
