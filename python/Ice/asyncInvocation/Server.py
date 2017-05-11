6e945603310ca788bbe4bdf57f3889bd3d7ddd85
InsertCreativityHere <austin.r.henriksen79@gmail.com>
2017-05-11 14:48:06 -0400

    python AsyncInvocation demo

---
+++ python/Ice/asyncInvocation/Server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# **********************************************************************
+#
+# Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+#
+# **********************************************************************
+
+import math, sys, Ice
+
+Ice.loadSlice('C:\\Users\\austin\\Desktop\\workspace\\python_AMI_demo\\Calculator.ice')
+Ice.updateModules()
+import Demo
+
+class CalculatorI(Demo._CalculatorDisp):
+    async def add(self, x, y, current=None):
+        return x + y
+    
+    async def subtract(self, x, subtrahend, current=None):
+        return x - subtrahend
+    
+    async def divide(self, numerator, denominator, current=None):
+        if(denominator == 0):
+            raise Demo.DivideByZeroException()
+        return (numerator // denominator, numerator % denominator)
+    
+    async def square(self, x, current=None):
+        return x * x
+    
+    async def squareRoot(self, x, current=None):
+        if(x < 0):
+            raise Demo.NegativeRootException()
+        return math.sqrt(x)
+    
+    async def shutdown(self, current=None):
+        current.adapter.getCommunicator().shutdown()
+
+class Server(Ice.Application):
+    def run(self, args):
+        
+        if len(args) > 1:
+            print(self.appName() + ": too many arguments")
+            return 1
+        
+        adapter = self.communicator().createObjectAdapter("Calculator")
+        adapter.add(CalculatorI(), Ice.stringToIdentity("calculator"))
+
+        adapter.activate()
+        
+        self.communicator().waitForShutdown()
+        return 0
+    
+sys.stdout.flush()
+app = Server()
+sys.exit(app.main(sys.argv, "config.server"))
