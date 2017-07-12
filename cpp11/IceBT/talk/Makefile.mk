4c7662bff87f306f2143328ade2fa2cf0c0defba
Jose <jose@zeroc.com>
2017-07-12 22:31:21 +0200

    ICE-8304 - Do not build IceBT demos if requirements are not installed

--- cpp11/IceBT/talk/Makefile.mk
+++ cpp11/IceBT/talk/Makefile.mk
@@ -9,6 +9,6 @@ $(demo)_dependencies    = IceBT Ice
 
 $(demo)_talk_sources    = App.cpp Talk.ice
 
-ifneq ($(filter debian ubuntu,$(linux_id)),)
+ifeq ($(shell pkg-config --exists bluez dbus-1 2> /dev/null && echo yes),yes)
 demos += $(demo)
 endif
