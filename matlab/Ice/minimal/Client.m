0adccef44d6e6024155496a48929d11333058ce2
Jose <jose@zeroc.com>
2017-10-17 10:44:19 +0200

    Add MATLAB minimal demo

---
+++ matlab/Ice/minimal/Client.m
@@ -0,0 +1,16 @@
+% **********************************************************************
+% Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+% **********************************************************************
+
+function Client(args)
+    addpath('generated');
+    if ~libisloaded('ice')
+        loadlibrary('ice', @iceproto)
+    end
+    import Demo.*
+
+    communicator = Ice.initialize(args);
+    cleanup = onCleanup(@() communicator.destroy());
+    hello = Demo.HelloPrx.checkedCast(communicator.stringToProxy('hello:default -h localhost -p 10000'));
+    hello.sayHello();
+end
