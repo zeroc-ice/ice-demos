dd746b8b85f9d9ca3d8d7eb60bdccc62e14ceee8
Jose <jose@zeroc.com>
2017-12-21 21:12:35 +0100

    Add missing files

---
+++ csharp/Manual/net45/lifecycle/server/NodeI.cs
@@ -0,0 +1,13 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2017 ZeroC, Inc. All rights reserved.
+//
+// **********************************************************************
+
+namespace FilesystemI
+{
+    public interface NodeI
+    {
+        Ice.Identity id();
+    }
+}
---
+++ csharp/Manual/net45/simpleFilesystem/server/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Manual simple filesystem demo server")]
+[assembly: AssemblyDescription("Manual simple filesystem demo server")]
+[assembly: AssemblyCompany("ZeroC, Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2003-2017 ZeroC, Inc.")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyProductAttribute("Ice")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
