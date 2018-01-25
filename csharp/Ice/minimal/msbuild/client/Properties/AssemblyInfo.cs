0fa7f20ab5394f5c9390ff734445b67a199edeae
Joe George <joe@zeroc.com>
2018-01-25 10:24:40 -0500

    Update copyright to 2018

--- csharp/Ice/minimal/msbuild/client/Properties/AssemblyInfo.cs
+++ csharp/Ice/minimal/msbuild/client/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ice minimal demo client")]
 [assembly: AssemblyDescription("Ice minimal demo client")]
 [assembly: AssemblyCompany("ZeroC, Inc.")]
-[assembly: AssemblyCopyright("Copyright (c) 2003-2017 ZeroC, Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2003-2018 ZeroC, Inc.")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyProductAttribute("Ice")]
