47181f5c295adc7ab1bc39ba0d1810d85534f754
Jose <jose@zeroc.com>
2019-05-13 22:03:53 +0200

    Manual/printer demo

---
+++ swift/Manual/printer/Server/main.swift
@@ -0,0 +1,33 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+import Ice
+import Foundation
+
+class PrinterI: Printer {
+    func printString(s: String, current: Ice.Current) {
+        print(s)
+    }
+}
+
+func run() -> Int32 {
+    do {
+        let communicator = try Ice.initialize(CommandLine.arguments)
+        defer {
+            communicator.destroy()
+        }
+        
+        let adapter = try communicator.createObjectAdapterWithEndpoints(name: "SimplePrinterAdapter", endpoints: "default -h localhost -p 10000")
+        try adapter.add(servant: PrinterI(), id: Ice.stringToIdentity("SimplePrinter"))
+        try adapter.activate()
+        
+        communicator.waitForShutdown()
+        return 0
+    } catch {
+        print("Error: \(error)\n")
+        return 1
+    }
+}
+
+exit(run())
