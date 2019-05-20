6b6ff20072469be2c40ff727fb8a9ff6a5a277d5
Jose <jose@zeroc.com>
2019-05-20 18:07:11 +0200

    Ice/asyncInvocation updates

--- swift/Ice/asyncInvocation/Client/main.swift
+++ swift/Ice/asyncInvocation/Client/main.swift
@@ -6,14 +6,6 @@ import Foundation
 import PromiseKit
 import Ice
 
-//
-// Run PromiseKit completion in .global queue so we can use the main
-// queue for wait f
-//
-PromiseKit.conf.Q.map = .global()
-PromiseKit.conf.Q.return = .global()
-PromiseKit.conf.logHandler = { _ in }
-
 func run() -> Int32 {
     do {
         var args = CommandLine.arguments
@@ -33,89 +25,70 @@ func run() -> Int32 {
                 return 1
         }
         
-        //
-        // Calculate 10 - 4 with an asynchronous call which in this case returns a Promise<Int32>
-        //
-        try Promise<Void> { seal in
-            firstly {
-                calculator.subtractAsync(x: 10, subtrahend: 4)
-            }.done { result in
-                print("10 minus 4 is \(result)")
-            }.catch { error in
-                seal.reject(error)
+        var status: Int32 = 0
+        firstly {
+            //
+            // Calculate 10 - 4 with an asynchronous call which in this case returns a Promise<Int32>
+            //
+            calculator.subtractAsync(x: 10, subtrahend: 4)
+        }.get { result in
+            print("10 minus 4 is \(result)")
+        }.then { _ in
+            //
+            // Calculate 13 / 5 asynchronously
+            //
+            calculator.divideAsync(numerator: 13, denominator: 5)
+        }.get { r in
+            // Since the divide operation has output parameters, the promise result is a tuple
+            // with the return value and the output parameters
+            print("13 / 5 is \(r.returnValue) with a remainder of \(r.remainder)")
+        }.recover { error -> Promise<(returnValue: Int32, remainder: Int32)> in
+            guard error is DivideByZeroException else {
+                throw error
             }
-        }.wait()
-
-        //
-        // Calculate 13 / 5 asynchronously
-        //
-        try Promise<Void> { seal in
-            calculator.divideAsync(numerator: 13, denominator: 5).done { r in
-                
-                // Since the divide operation has output parameters, the promise result is a tuple
-                // with the return value and the output parameters
-                print("13 / 5 is \(r.returnValue) with a remainder of \(r.remainder)")
-                seal.fulfill(())
-            }.catch { error in
-                if error is DivideByZeroException {
-                    // Don't let this exception stop the rest of the program's execution
-                    print("You cannot divide by zero")
-                    seal.fulfill(())
-                } else {
-                    // Allow this exception to terminate the program's execution
-                    seal.reject(error)
-                }
+            print("You cannot divide by zero")
+            return .value((returnValue: 0, remainder: 0))
+        }.then { _ in
+            //
+            // Same with 13 / 0
+            //
+            calculator.divideAsync(numerator: 13, denominator: 0)
+        }.get { r in
+            // Since the divide operation has output parameters, the promise result is a tuple
+            // with the return value and the output parameters
+            print("13 / 0 is \(r.returnValue) with a remainder of \(r.remainder)")
+        }.recover { error -> Promise<(returnValue: Int32, remainder: Int32)> in
+            guard error is DivideByZeroException else {
+                throw error
             }
-        }.wait()
-        
-        //
-        // Same with 13 / 0
-        //
-        try Promise<Void> { seal in
-            calculator.divideAsync(numerator: 13, denominator: 0).done { r in
-                
-                // Since the divide operation has output parameters, the promise result is a tuple
-                // with the return value and the output parameters
-                print("13 / 0 is \(r.returnValue) with a remainder of \(r.remainder)")
-                seal.fulfill(())
-            }.catch { error in
-                if error is DivideByZeroException {
-                    // Don't let this exception stop the rest of the program's execution
-                    print("You cannot divide by zero")
-                    seal.fulfill(())
-                } else {
-                    // Allow this exception to terminate the program's execution
-                    seal.reject(error)
-                }
+            print("You cannot divide by zero")
+            return .value((returnValue: 0, remainder: 0))
+        }.then { _ in
+            //
+            // Have the calculator find the hypotenuse of a triangle with side lengths of 6 and 8
+            // using the Pythagorean theorem and chained asynchronous tasks.
+            //
+            when(fulfilled: calculator.squareAsync(6), calculator.squareAsync(8))
+        }.then { side1, side2 in
+            calculator.addAsync(x: side1, y: side2)
+        }.then { sum in
+            calculator.squareRootAsync(sum)
+        }.get { hypotenuse in
+            print("The hypotenuse of a triangle with side lengths of 6 and 8 is \(hypotenuse)")
+        }.then { _ in
+            calculator.shutdownAsync()
+        }.catch { error in
+            if error is DivideByZeroException {
+                print("You cannot take the square root of a negative number")
+            } else {
+                print("Error: \(error)")
             }
-        }.wait()
-        
-        //
-        // Have the calculator find the hypotenuse of a triangle with side lengths of 6 and 8
-        // using the Pythagorean theorem and chained asynchronous tasks.
-        //
-        try Promise<Void> { seal in
-            firstly {
-                when(fulfilled: calculator.squareAsync(6), calculator.squareAsync(8))
-            }.then { side1, side2 in
-                calculator.addAsync(x: side1, y: side2)
-            }.then { sum in
-                calculator.squareRootAsync(sum)
-            }.done { hypotenuse in
-                print("The hypotenuse of a triangle with side lengths of 6 and 8 is \(hypotenuse)")
-                seal.fulfill(())
-            }.catch {error in
-                if error is DivideByZeroException {
-                    print("You cannot take the square root of a negative number")
-                    seal.fulfill(())
-                } else {
-                    seal.reject(error)
-                }
-            }
-        }.wait()
-
-        try calculator.shutdown()
-
+            status = 1
+        }.finally {
+            communicator.destroy()
+            exit(status)
+        }
+        RunLoop.main.run()
         return 0
     } catch {
         print("Error: \(error)")
