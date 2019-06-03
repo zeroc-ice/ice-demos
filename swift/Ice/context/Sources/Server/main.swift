34a480a015a083b93b2d981256dbc704bae4a867
Jose <jose@zeroc.com>
2019-06-03 16:36:57 +0200

    Add Ice/context demo

---
+++ swift/Ice/context/Sources/Server/main.swift
@@ -0,0 +1,33 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+import Ice
+import Foundation
+
+func run() -> Int32 {
+    do {
+        var args = CommandLine.arguments
+        let communicator = try Ice.initialize(args: &args, configFile: "config.server")
+        defer {
+            communicator.destroy()
+        }
+
+        guard args.count == 1 else {
+            print("too many arguments\n")
+            return 1
+        }
+
+        let adapter = try communicator.createObjectAdapter("Context")
+        try adapter.add(servant: ContextI(), id: Ice.stringToIdentity("context"))
+        try adapter.activate()
+
+        communicator.waitForShutdown()
+    } catch {
+        print("Error: \(error)\n")
+        return 1
+    }
+    return 0
+}
+
+exit(run())
