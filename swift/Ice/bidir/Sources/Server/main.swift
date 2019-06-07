4d7190d65aec88d35d56c66bd7298cd17d6f1610
Joe George <joe@zeroc.com>
2019-06-07 16:16:48 -0400

    Remove underscore from swift/Ice/bidir

--- swift/Ice/bidir/Sources/Server/main.swift
+++ swift/Ice/bidir/Sources/Server/main.swift
@@ -2,33 +2,32 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 //
 
-import Ice
 import Foundation
+import Ice
 import PromiseKit
 
 class CallbackSenderI: CallbackSender {
-
-    var _clients: [CallbackReceiverPrx] = []
-    var _num: Int32 = 0
+    var clients: [CallbackReceiverPrx] = []
+    var num: Int32 = 0
 
     func addClient(receiver: CallbackReceiverPrx?, current: Ice.Current) throws {
         if let client = receiver {
             DispatchQueue.global(qos: .background).async(flags: .barrier) {
                 print("adding client `\(Ice.identityToString(id: client.ice_getIdentity()))'")
-                self._clients.append(client.ice_fixed(current.con!))
+                self.clients.append(client.ice_fixed(current.con!))
             }
         }
     }
 
     func dispatch() {
-        if _clients.count > 0 {
-            _num += 1
-            for c in _clients {
-                c.callbackAsync(_num).catch { e in
+        if clients.count > 0 {
+            num += 1
+            for c in clients {
+                c.callbackAsync(num).catch { e in
                     DispatchQueue.global(qos: .background).async(flags: .barrier) {
                         precondition(e is Ice.Exception)
                         print("removing client \(Ice.identityToString(id: c.ice_getIdentity())):\n:\(e)")
-                        self._clients.removeAll { $0 == c }
+                        self.clients.removeAll { $0 == c }
                     }
                 }
             }
