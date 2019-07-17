5a0f345fc13fffe313c5d2afb319d68d003a4d3a
Austin Henriksen <austin@zeroc.com>
2019-07-17 13:26:32 -0400

    Added dedicated token lock, and changed token generation.

--- java/Ice/interceptor/AuthenticatorI.java
+++ java/Ice/interceptor/AuthenticatorI.java
@@ -12,60 +12,68 @@ public class AuthenticatorI implements Authenticator
     }
 
     @Override
-    public synchronized Token getToken(String username, String password, com.zeroc.Ice.Current current)
+    public Token getToken(String username, String password, com.zeroc.Ice.Current current)
     {
         //
-        // Generate a random 32 character long hex string to use as a token.
+        // Generate a random 32 character long token.
         //
         java.util.Random rand = new java.util.Random();
-        StringBuilder tokenBuilder = new StringBuilder();
-        while(tokenBuilder.length() < 32)
+        String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+        StringBuilder tokenBuilder = new StringBuilder(32);
+        for(int i = 0; i < 32; i++)
         {
-            tokenBuilder.append(Integer.toHexString(rand.nextInt()));
+            tokenBuilder.append(chars.charAt(rand.nextInt(chars.length())));
         }
 
         //
         // By default tokens are valid for 1 hour after being issued.
         //
-        long expireTime = System.currentTimeMillis() + tokenLifetime;
+        long expireTime = System.currentTimeMillis() + TOKEN_LIFETIME;
 
         //
         // Create a token object, store a copy, and return it.
         //
         Token token = new Token(tokenBuilder.toString(), username, expireTime);
-        _tokenStore.add(token.clone());
+        synchronized(_tokenLock)
+        {
+            _tokenStore.add(token.clone());
+        }
         System.out.println("Issuing new access token for user: " + username + ". Token=" + token.id);
         return token;
     }
 
     @Override
-    public synchronized void validateToken(String tokenId, com.zeroc.Ice.Current current)
+    public void validateToken(String tokenId, com.zeroc.Ice.Current current)
         throws AuthorizationException
     {
-        for(Token token : _tokenStore)
+        synchronized(_tokenLock)
         {
-            //
-            // Check if the authenticator has issued any tokens with a matching Id.
-            //
-            if(token.id.equals(tokenId))
+            for(Token token : _tokenStore)
             {
                 //
-                // Delete the token if it has expired, otherwise return true.
+                // Check if the authenticator has issued any tokens with a matching Id.
                 //
-                if(token.expireTime <= System.currentTimeMillis())
+                if(token.id.equals(tokenId))
                 {
-                    _tokenStore.remove(token);
-                    throw new TokenExpiredException();
-                }
-                else
-                {
-                    return;
+                    //
+                    // Delete the token if it has expired.
+                    //
+                    if(token.expireTime <= System.currentTimeMillis())
+                    {
+                        _tokenStore.remove(token);
+                        throw new TokenExpiredException();
+                    }
+                    else
+                    {
+                        return;
+                    }
                 }
             }
+            throw new AuthorizationException();
         }
-        throw new AuthorizationException();
     }
 
-    public static final long tokenLifetime = 1000 * 60 * 60;
+    public static final long TOKEN_LIFETIME = 1000 * 60 * 60;
     private final java.util.List<Token> _tokenStore;
+    private final Object _tokenLock = new Object();
 }
