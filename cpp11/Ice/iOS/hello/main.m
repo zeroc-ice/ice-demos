b9632673a7f6aa3d94febd1717bc6821e2b464ac
Reece Humphreys <reecewh@icloud.com>
2019-07-30 11:21:45 -0400

    Updated Autolayout for UI controls

--- cpp11/Ice/iOS/hello/main.m
+++ cpp11/Ice/iOS/hello/main.m
@@ -4,6 +4,15 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation CALayer (Additions)
+- (void)setBorderColorFromUIColor:(UIColor *)color
+{
+    self.borderColor = color.CGColor;
+    NSLog(@"Got here and called the function");
+}
+@end
 
 int main(int argc, char *argv[])
 {
