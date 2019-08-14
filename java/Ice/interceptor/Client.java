19d9a0fa3b3028436360148138c004367f774a8f
Austin Henriksen <austin@zeroc.com>
2019-08-14 14:39:07 -0400

    Made java demo interactive (built & tested).

--- java/Ice/interceptor/Client.java
+++ java/Ice/interceptor/Client.java
@@ -36,6 +36,7 @@ class Client
 
     private static int run(com.zeroc.Ice.Communicator communicator)
     {
+        com.zeroc.Ice.ImplicitContext context = communicator.getImplicitContext();
         ThermostatPrx thermostat =
             ThermostatPrx.checkedCast(communicator.propertyToProxy("Thermostat.Proxy"));
         if(thermostat == null)
@@ -44,48 +45,6 @@ class Client
             return 1;
         }
 
-        //
-        // Read in values from the command line for the username, password, and
-        // the temperature to try and set the thermostat to.
-        //
-        String username;
-        String password;
-        float desiredTemp;
-        try(java.util.Scanner userIn = new java.util.Scanner(System.in))
-        {
-            System.out.println("Username:");
-            username = userIn.nextLine();
-            System.out.println("Password:");
-            password = userIn.nextLine();
-            System.out.println("Enter desired temperature:");
-            desiredTemp = Float.parseFloat(userIn.nextLine());
-            System.out.println();
-        }
-        catch(NumberFormatException ex)
-        {
-            System.err.println("Specified temperature is not a valid float.");
-            return 1;
-        }
-
-        //
-        // Tries calling 'setTemp' without authorization and receives an exception.
-        //
-        System.out.println("Attempting to set temperature without access token...");
-        try
-        {
-            System.out.println("\tCurrent temperature is " + thermostat.getTemp());
-            thermostat.setTemp(desiredTemp);
-            System.out.println("\tNew temperature is " + thermostat.getTemp());
-        }
-        catch(AuthorizationException ex)
-        {
-            System.err.println("\tFailed to set temperature. Access denied!");
-        }
-        System.out.println();
-
-        //
-        // Request an access token from the server's authentication object.
-        //
         AuthenticatorPrx authenticator =
             AuthenticatorPrx.checkedCast(communicator.propertyToProxy("Authenticator.Proxy"));
         if(authenticator == null)
@@ -93,34 +52,113 @@ class Client
             System.err.println("invalid authenticator proxy");
             return 1;
         }
-        Token token = authenticator.getToken(username, password);
-        System.out.println("Successfully retrieved access token: \"" + token.value + "\"\n");
 
-        //
-        // Add the access token to the communicator's context, so it will be
-        // sent along with every request made through it.
-        //
-        com.zeroc.Ice.ImplicitContext context = communicator.getImplicitContext();
-        context.put("accessToken", token.value);
+        menu();
 
-        //
-        // Tries calling 'setTemp' again, this time with the access token.
-        //
-        System.out.println("Attempting to set temperature with access token...");
-        try
+        java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+
+        String line = null;
+        do
         {
-            System.out.println("\tCurrent temperature is " + thermostat.getTemp());
-            thermostat.setTemp(desiredTemp);
-            System.out.println("\tNew temperature is " + thermostat.getTemp());
+            try
+            {
+                System.out.print("\n==> ");
+                System.out.flush();
+                line = in.readLine();
+                if(line == null)
+                {
+                    break;
+                }
+                switch(line)
+                {
+                    case "get-temp":
+                    {
+                        System.out.println("Current temperature is " + thermostat.getTemp());
+                        break;
+                    }
+                    case "set-temp":
+                    {
+                        try
+                        {
+                            System.out.println("Enter desired temperature: ");
+                            thermostat.setTemp(Float.parseFloat(in.readLine()));
+                            System.out.println("New temperature is " + thermostat.getTemp());
+                        }
+                        catch(NumberFormatException ex)
+                        {
+                            System.err.println("Provided temperature is not a parsable float.");
+                        }
+                        catch(AuthorizationException ex)
+                        {
+                            System.err.println("Failed to set temperature. Access denied!");
+                        }
+                        break;
+                    }
+                    case "get-token":
+                    {
+                        System.out.println("Username: ");
+                        String username = in.readLine();
+                        System.out.println("Password: ");
+                        String password = in.readLine();
+                        //
+                        // Request an access token from the server's authentication object.
+                        //
+                        Token token = authenticator.getToken(username, password);
+                        System.out.println("Successfully retrieved access token: \"" + token.value + "\"\n");
+                        //
+                        // Add the access token to the communicator's context, so it will be
+                        // sent along with every request made through it.
+                        //
+                        context.put("accessToken", token.value);
+                        break;
+                    }
+                    case "release-token":
+                    {
+                        context.remove("accessToken");
+                        break;
+                    }
+                    case "shutdown":
+                    {
+                        thermostat.shutdown();
+                        break;
+                    }
+                    case "x":
+                    {
+                        // Nothing to do
+                        break;
+                    }
+                    case "?":
+                        menu();
+                        break;
+                    default:
+                        System.out.println("unknown command `" + line + "'");
+                        menu();
+                }
+            }
+            catch(java.io.IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            catch(com.zeroc.Ice.LocalException ex)
+            {
+                ex.printStackTrace();
+            }
         }
-        catch(AuthorizationException ex)
-        {
-            //
-            // No longer encountered since the client has authorization to use 'setTemp' now.
-            //
-            System.err.println("\tFailed to set temperature. Access denied!");
-        }
+        while(!line.equals("x"));
 
         return 0;
+    }
+
+    private static void menu()
+    {
+        System.out.println(
+            "usage:\n" +
+            "get-temp: gets the current temperature\n" +
+            "set-temp: sets the temperature\n" +
+            "get-token: gets a new token from the server\n" +
+            "release-token: releases the currently held token\n" +
+            "shutdown: shutdown server\n" +
+            "x: exit\n" +
+            "?: help\n");
     }
 }
