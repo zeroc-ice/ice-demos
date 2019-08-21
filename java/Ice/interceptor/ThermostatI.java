d134cf3580e746a2ad5626cc677e209869c496b9
Austin Henriksen <austin@zeroc.com>
2019-08-21 16:23:49 -0400

    Marked thermostat temperature as volatile for memory synchronicity.

--- java/Ice/interceptor/ThermostatI.java
+++ java/Ice/interceptor/ThermostatI.java
@@ -3,28 +3,29 @@
 //
 
 import com.zeroc.demos.Ice.interceptor.Demo.*;
+import com.zeroc.Ice.Current;
 
 class ThermostatI implements Thermostat
 {
     @Override
-    public float getTemp(com.zeroc.Ice.Current current)
+    public float getTemp(Current current)
     {
         return _temperature;
     }
 
     @Override
-    public void setTemp(float temp, com.zeroc.Ice.Current current)
+    public void setTemp(float temp, Current current)
     {
         _temperature = temp;
     }
 
     @Override
-    public void shutdown(com.zeroc.Ice.Current current)
+    public void shutdown(Current current)
     {
         System.out.println("Shutting down...");
         current.adapter.getCommunicator().shutdown();
     }
 
     // Temperature in Celsius.
-    private float _temperature = 23.5f;
+    private volatile float _temperature = 23.5f;
 }
