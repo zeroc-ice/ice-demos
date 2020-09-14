43826017cb35ee3ee2929412b1e53273f5563899
Jose <jose@zeroc.com>
2020-09-14 12:10:45 +0200

    Port callback and optional demos

---
+++ csharp/Ice/callback/client/Client.cs
@@ -0,0 +1,81 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using Demo;
+using System;
+using System.Configuration;
+using ZeroC.Ice;
+
+// The new communicator is automatically destroyed (disposed) at the end of the using statement
+
+using var communicator = new Communicator(ref args, ConfigurationManager.AppSettings);
+ 
+// The communicator initialization removes all Ice-related arguments from args
+if(args.Length > 0)
+{
+    throw new ArgumentException("too many arguments");
+}
+
+
+var sender = communicator.GetPropertyAsProxy("CallbackSender.Proxy", ICallbackSenderPrx.Factory) ??
+    throw new ArgumentException("invalid proxy");
+
+var adapter = communicator.CreateObjectAdapter("Callback.Client");
+var receiver = adapter.Add("callbackReceiver", new CallbackReceiver(), ICallbackReceiverPrx.Factory);
+adapter.Activate();
+
+Menu();
+
+string line = null;
+do
+{
+    try
+    {
+        Console.Out.Write("==> ");
+        Console.Out.Flush();
+        line = Console.In.ReadLine();
+        if(line == null)
+        {
+            break;
+        }
+        if(line.Equals("t"))
+        {
+            sender.InitiateCallback(receiver);
+        }
+        else if(line.Equals("s"))
+        {
+            sender.Shutdown();
+        }
+        else if(line.Equals("x"))
+        {
+            // Nothing to do
+        }
+        else if(line.Equals("?"))
+        {
+            Menu();
+        }
+        else
+        {
+            Console.Out.WriteLine("unknown command `" + line + "'");
+            Menu();
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.Error.WriteLine(ex);
+    }
+}
+while(!line.Equals("x"));
+
+static void Menu()
+{
+    Console.Out.Write(
+@"usage:
+    t: send callback
+    s: shutdown server
+    x: exit
+    ?: help
+");
+}
+
---
+++ csharp/Ice/optional/client/Client.cs
@@ -0,0 +1,177 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using Demo;
+using System;
+using System.Configuration;
+using ZeroC.Ice;
+
+
+// using statement - communicator is automatically destroyed at the end of this statement
+using var communicator = new Communicator(ref args, ConfigurationManager.AppSettings);
+
+// Destroy the communicator on Ctrl+C or Ctrl+Break
+Console.CancelKeyPress += (sender, eventArgs) => communicator.DisposeAsync();
+
+if (args.Length > 0)
+{
+    throw new ArgumentException("too many arguments");
+}
+
+IContactDBPrx contactdb = communicator.GetPropertyAsProxy("ContactDB.Proxy", IContactDBPrx.Factory) ??
+    throw new ArgumentException("invalid proxy");
+
+// Add a contact for "john". All parameters are provided.
+string johnNumber = "123-456-7890";
+contactdb.AddContact("john", NumberType.HOME, johnNumber, 0);
+
+Console.Write("Checking john... ");
+
+
+// Find the phone number for "john".
+string number = contactdb.QueryNumber("john");
+
+// HasValue tests if an optional value is set.
+if (number != johnNumber)
+{
+    Console.Write("number is incorrect ");
+}
+
+// Optional can also be used in an out parameter.
+int? dialgroup =  contactdb.QueryDialgroup("john");
+if (!dialgroup.HasValue || dialgroup.Value != 0)
+{
+    Console.Write("dialgroup is incorrect ");
+}
+
+Contact info = contactdb.Query("john");
+
+
+// All of the info parameters should be set. Each of the optional members of the class map to Ice.Optional<T> member.
+if (!info.Type.HasValue || !info.DialGroup.HasValue)
+{
+    Console.Write("info is incorrect ");
+}
+else if (info.Type.Value != NumberType.HOME || info.Number != johnNumber || info.DialGroup.Value != 0)
+{
+    Console.Write("info is incorrect ");
+}
+Console.WriteLine("ok");
+
+
+// Add a contact for "steve". The behavior of the server is to default construct the Contact, and then assign  all set
+// parameters. Since the default value of NumberType in the slice definition is NumberType.HOME and in this case the
+// NumberType is unset it will take the default value.
+
+string steveNumber = "234-567-8901";
+contactdb.AddContact("steve", null, steveNumber, 1);
+
+Console.Write("Checking steve... ");
+number = contactdb.QueryNumber("steve");
+if (number != steveNumber)
+{
+    Console.Write("number is incorrect ");
+}
+
+info = contactdb.Query("steve");
+
+// Check the value for the NumberType.
+if (info.Type.HasValue)
+{
+    Console.Write("info is incorrect ");
+}
+
+if (info.Number != steveNumber || info.DialGroup.Value != 1)
+{
+    Console.Write("info is incorrect ");
+}
+
+dialgroup = contactdb.QueryDialgroup("steve");
+if (!dialgroup.HasValue || dialgroup.Value != 1)
+{
+    Console.Write("dialgroup is incorrect ");
+}
+
+Console.WriteLine("ok");
+
+// Add a contact from "frank". Here the dialGroup field isn't set.
+string frankNumber = "345-678-9012";
+contactdb.AddContact("frank", NumberType.CELL, frankNumber, null);
+
+Console.Write("Checking frank... ");
+
+number = contactdb.QueryNumber("frank");
+if (number != frankNumber)
+{
+    Console.Write("number is incorrect ");
+}
+
+info = contactdb.Query("frank");
+
+// The dial group field should be unset.
+if (info.DialGroup.HasValue)
+{
+    Console.Write("info is incorrect ");
+}
+
+if (info.Type.Value != NumberType.CELL || info.Number != frankNumber)
+{
+    Console.Write("info is incorrect ");
+}
+
+dialgroup = contactdb.QueryDialgroup("frank");
+if (dialgroup.HasValue)
+{
+    Console.Write("dialgroup is incorrect ");
+}
+Console.WriteLine("ok");
+
+
+// Add a contact from "anne". The number field isn't set.
+contactdb.AddContact("anne", NumberType.OFFICE, null, 2);
+
+Console.Write("Checking anne... ");
+number = contactdb.QueryNumber("anne");
+if (number != null)
+{
+    Console.Write("number is incorrect ");
+}
+
+info = contactdb.Query("anne");
+
+// The number field should be unset.
+if (info.Number != null)
+{
+    Console.Write("info is incorrect ");
+}
+
+if (info.Type.Value != NumberType.OFFICE || info.DialGroup.Value != 2)
+{
+    Console.Write("info is incorrect ");
+}
+
+dialgroup = contactdb.QueryDialgroup("anne");
+if (!dialgroup.HasValue || dialgroup.Value != 2)
+{
+    Console.Write("dialgroup is incorrect ");
+}
+
+// The optional fields can be used to determine what fields to update on the contact. Here we update only the number
+// for anne, the remainder of the fields are unchanged.
+string anneNumber = "456-789-0123";
+contactdb.UpdateContact("anne", null, anneNumber, null);
+number = contactdb.QueryNumber("anne");
+if (number != anneNumber)
+{
+    Console.Write("number is incorrect ");
+}
+
+info = contactdb.Query("anne");
+if (info.Number != anneNumber || info.Type.Value != NumberType.OFFICE || info.DialGroup.Value != 2)
+{
+    Console.Write("info is incorrect ");
+}
+Console.WriteLine("ok");
+
+contactdb.Shutdown();
