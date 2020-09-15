e9342991fae8bc5c6fb044ceec0c4f0e79366479
Jose <jose@zeroc.com>
2020-09-15 10:40:48 +0200

    More C# demos

---
+++ csharp/IceBox/hello/helloservice/HelloService.cs
@@ -0,0 +1,28 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using ZeroC.Ice;
+using ZeroC.IceBox;
+
+namespace Demo
+{
+    class HelloService : IService
+    {
+        private ObjectAdapter _adapter;
+
+        public void Start(string name, Communicator communicator, string[] args)
+        {
+            _adapter = communicator.CreateObjectAdapter(name);
+            _adapter.Add("hello", new Hello());
+            _adapter.Activate();
+        }
+
+        public void Stop() => _adapter.DisposeAsync();
+    }
+
+    public class Hello : IHello
+    {
+        public void SayHello(Current current) => System.Console.WriteLine("Hello World!");
+    }
+}
