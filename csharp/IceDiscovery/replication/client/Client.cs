10551d0cca4ae3a554daf3770990ceb34806ab74
Jose <jose@zeroc.com>
2020-09-15 17:18:17 +0200

    IceDiscovery/replication demo

---
+++ csharp/IceDiscovery/replication/client/Client.cs
@@ -0,0 +1,55 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using Demo;
+using System;
+using System.Configuration;
+using ZeroC.Ice;
+
+// The new communicator is automatically destroyed (disposed) at the end of the using statement
+using var communicator = new Communicator(ref args, ConfigurationManager.AppSettings);
+
+// The communicator initialization removes all Ice-related arguments from args
+if (args.Length > 0)
+{
+    throw new ArgumentException("too many arguments");
+}
+
+// Get the hello proxy. We configure the proxy to not cache the server connection with the proxy and to disable the
+// locator cache. With this configuration, the IceGrid locator will be queried for each invocation on the proxy and
+// the invocation will be sent over the server connection matching the returned endpoints.
+var hello = IHelloPrx.Parse("hello", communicator);
+hello = hello.Clone(cacheConnection: false, locatorCacheTimeout: TimeSpan.Zero);
+
+while (true)
+{
+    Console.Out.Write("enter the number of iterations: ");
+    Console.Out.Flush();
+    string? line = Console.In.ReadLine();
+    if(line == null || line == "x")
+    {
+        break;
+    }
+    int count = int.Parse(line);
+
+    Console.Out.Write("enter the delay between each greetings (in ms): ");
+    Console.Out.Flush();
+    line = Console.In.ReadLine();
+    if(line == null || line == "x")
+    {
+        break;
+    }
+    int delay = Convert.ToInt32(line);
+
+    if(delay < 0)
+    {
+        delay = 500; // 500 milli-seconds
+    }
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Out.WriteLine(hello.GetGreeting());
+        System.Threading.Thread.Sleep(delay);
+    }
+}
---
+++ csharp/IceDiscovery/replication/server/Server.cs
@@ -0,0 +1,23 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using Demo;
+using System;
+using ZeroC.Ice;
+
+// using statement - communicator is automatically destroyed at the end of this statement.
+using var communicator = new Communicator(ref args);
+
+// Destroy the communicator on Ctrl+C or Ctrl+Break
+Console.CancelKeyPress += (sender, eventArgs) => communicator.DisposeAsync();
+
+if(args.Length > 0)
+{
+    throw new ArgumentException("too many arguments");
+}
+
+ObjectAdapter adapter = communicator.CreateObjectAdapter("Hello");
+adapter.Add("hello", new Hello(communicator.GetProperty("Ice.ProgramName")!));
+adapter.Activate();
+communicator.WaitForShutdown();
