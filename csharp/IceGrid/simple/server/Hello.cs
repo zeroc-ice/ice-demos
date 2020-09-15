66a6b2bfa1f7081f62c0c5ef64977ddd5435c5d8
Jose <jose@zeroc.com>
2020-09-15 13:33:52 +0200

    IceGrid simple demo

---
+++ csharp/IceGrid/simple/server/Hello.cs
@@ -0,0 +1,24 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using System;
+using ZeroC.Ice;
+
+namespace Demo
+{
+    public class Hello : IHello
+    {
+        private string _name;
+        public Hello(string name) => _name = name;
+
+        public void SayHello(Current current) => Console.WriteLine($"{_name} says Hello World!");
+
+        public void Shutdown(Current current)
+        {
+            Console.WriteLine($"{_name} shutting down...");
+            current.Communicator.DisposeAsync();
+        }
+    }
+
+}
---
+++ csharp/IceGrid/simple/server/Server.cs
@@ -0,0 +1,24 @@
+//
+// Copyright (c) ZeroC, Inc. All rights reserved.
+//
+
+using Demo;
+using System;
+using System.Configuration;
+using ZeroC.Ice;
+
+// using statement - communicator is automatically destroyed at the end of this statement.
+using var communicator = new Communicator(ref args, ConfigurationManager.AppSettings);
+
+// Destroy the communicator on Ctrl+C or Ctrl+Break
+Console.CancelKeyPress += (sender, eventArgs) => communicator.DisposeAsync();
+
+if(args.Length > 0)
+{
+    throw new ArgumentException("too many arguments");
+}
+
+var adapter = communicator.CreateObjectAdapter("Hello");
+adapter.Add(communicator.GetProperty("Identity"), new Hello(communicator.GetProperty("Ice.ProgramName")));
+adapter.Activate();
+communicator.WaitForShutdown();
