fc06e2f94fa355095f3416018649b2fde4b182eb
Reece Humphreys <reecewh@icloud.com>
2022-04-11 15:01:10 -0400

    Removed Objective-C UI demos

--- objective-c/Chat/Cocoa/main.m
+++
@@ -1,9 +0,0 @@
-//
-// Copyright (c) ZeroC, Inc. All rights reserved.
-//
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc,  (const char **) argv);
-}
