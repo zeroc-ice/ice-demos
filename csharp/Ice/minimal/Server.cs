4c4f4e4dae2cede95bed02435bd7f4041d95b95c
Bernard Normier <bernard@zeroc.com>
2023-10-06 10:18:46 -0400

    Update minimal C# demo (#195)

--- csharp/Ice/minimal/Server.cs
+++ csharp/Ice/minimal/Server.cs
@@ -12,12 +12,15 @@ public class Server
         {
             using(var communicator = Ice.Util.initialize(ref args))
             {
-                //
-                // Destroy the communicator on Ctrl+C or Ctrl+Break
-                //
-                Console.CancelKeyPress += (sender, eventArgs) => communicator.destroy();
+                // Shut down the communicator on Ctrl+C or Ctrl+Break.
+                Console.CancelKeyPress += (sender, eventArgs) =>
+                {
+                    eventArgs.Cancel = true;
+                    communicator.shutdown();
+                };
+
                 var adapter = communicator.createObjectAdapterWithEndpoints("Hello", "default -h localhost -p 10000");
-                adapter.add(new HelloI(), Ice.Util.stringToIdentity("hello"));
+                adapter.add(new Printer(), Ice.Util.stringToIdentity("hello"));
                 adapter.activate();
                 communicator.waitForShutdown();
             }
