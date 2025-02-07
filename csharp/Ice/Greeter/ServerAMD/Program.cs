30447cfff2749008405128a5cb971ae9f37d1ee3
Bernard Normier <bernard@zeroc.com>
2025-02-07 09:05:28 -0500

    Add Secure demo in C# (#272)

--- csharp/Ice/Greeter/ServerAMD/Program.cs
+++ csharp/Ice/Greeter/ServerAMD/Program.cs
@@ -6,13 +6,15 @@ using GreeterServerAMD;
 using Ice.Communicator communicator = Ice.Util.initialize(ref args);
 
 // Create an object adapter that listens for incoming requests and dispatches them to servants.
-var adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061");
+Ice.ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061");
 
 // Register the Chatbot servant with the adapter.
 adapter.add(new Chatbot(), Ice.Util.stringToIdentity("greeter"));
 
 // Start dispatching requests.
 adapter.activate();
+Console.WriteLine("Listening on port 4061...");
 
 // Wait until the user presses Ctrl+C.
 await CancelKeyPressed;
+Console.WriteLine("Caught Ctrl+C, exiting...");
