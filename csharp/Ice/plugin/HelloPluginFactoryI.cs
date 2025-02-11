c552627f9bb0bd4b735ce8640b6332d69a04c563
Bernard Normier <bernard@zeroc.com>
2025-02-11 16:00:32 -0500

    Remove Ice/hello, Ice/async and more C# demos (#287)

--- csharp/Ice/plugin/HelloPluginFactoryI.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) ZeroC, Inc.
-
-internal class HelloPluginFactoryI : Ice.PluginFactory
-{
-    public Ice.Plugin create(Ice.Communicator communicator, string name, string[] args)
-    {
-        return new HelloPluginI(communicator);
-    }
-}
--- csharp/Ice/plugin/LoggerPluginFactoryI.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) ZeroC, Inc.
-
-internal class LoggerPluginFactoryI : Ice.PluginFactory
-{
-    public Ice.Plugin create(Ice.Communicator communicator, string name, string[] args)
-    {
-        return new Ice.LoggerPlugin(communicator, new LoggerI());
-    }
-}
