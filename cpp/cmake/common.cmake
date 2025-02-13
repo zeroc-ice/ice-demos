90ddb16bbbacb7b9c5ff3354671f12e9091e9251
Joe George <joe@zeroc.com>
2025-02-13 11:36:35 -0500

    Add CXX build flags

--- cpp/cmake/common.cmake
+++ cpp/cmake/common.cmake
@@ -1,7 +1,24 @@
-cmake_minimum_required(VERSION 3.16)
+cmake_minimum_required(VERSION 3.24)
 
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+option(CMAKE_COMPILE_WARNING_AS_ERROR "Compile warnings as errors" ON)
+
+# We use these flags over presets to avoid having to create a CMakePreset.json file in every project.
+# If you want to disable this behavior, set the option to OFF: -DUSE_ICE_CXX_FLAGS=OFF
+option(USE_ICE_CXX_FLAGS "Common C++ compiler flags" ON)
+if(USE_ICE_CXX_FLAGS)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(flags -Wall -Wall -Wextra -Wredundant-decls -Wshadow -Wdeprecated)
+    elseif(flags STREQUAL "Clang")
+        set(ICE_CXX_FLAGS -Wall -Wextra -Wshadow -Wredundant-decls -Wno-shadow-field -Wdeprecated -Wstrict-prototypes -Wconversion -Wdocumentation)
+    elseif(flags STREQUAL "MSVC")
+        set(ICE_CXX_FLAGS /W4)
+    endif()
+    add_compile_options(${flags})
+    unset(flags)
+endif()
 
 find_package(Threads REQUIRED)
 
