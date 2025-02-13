acae0ad7932e6962dd49646a24222719251b6a8c
Bernard Normier <bernard@zeroc.com>
2025-02-13 11:14:08 -0500

    New Middleware demo in C#

---
+++ csharp/Ice/Middleware/README.md
@@ -0,0 +1,37 @@
+# Middleware
+
+The Middleware demo shows how to write a simple middleware and how to add this middleware to an object adapter.
+
+Each object adapter maintains a dispatch pipeline:
+
+```mermaid
+flowchart LR
+    client[Client] -- request --> am -- response --> client
+    subgraph pipeline [Dispatch pipeline]
+        direction LR
+        am[Authorization <br>middleware] --> om[Other <br>middleware] --> Servant --> om --> am
+    end
+```
+
+> [!NOTE]
+> Other middleware is just an example, and is not provided by this demo.
+
+You can build the client and server applications with:
+
+``` shell
+dotnet build
+```
+
+First start the Server program:
+
+```shell
+cd Server
+dotnet run
+```
+
+In a separate terminal, start the Client program:
+
+```shell
+cd Client
+dotnet run
+```
---
+++ csharp/Ice/Middleware/Server/AuthorizationMiddleware.cs
@@ -0,0 +1,38 @@
+// Copyright (c) ZeroC, Inc.
+
+namespace Server;
+
+/// <summary>The AuthorizationMiddleware intercepts incoming requests and checks the token entry in their contexts.
+/// </summary>
+internal class AuthorizationMiddleware : Ice.Object
+{
+    private readonly Ice.Object _next;
+    private readonly string _validToken;
+
+    /// <inheritdoc />
+    public ValueTask<Ice.OutgoingResponse> dispatchAsync(Ice.IncomingRequest request)
+    {
+        // Check if the request has a valid token in its context.
+        if (!request.current.ctx.TryGetValue("token", out string? token) || token != _validToken)
+        {
+            Console.WriteLine($"Rejecting request with token '{token}'");
+
+            // Ice transmits this ObjectNotExistException to this client.
+            throw new Ice.ObjectNotExistException();
+        }
+
+        Console.WriteLine($"Accepting request with token '{token}'");
+
+        // Continue the dispatch pipeline.
+        return _next.dispatchAsync(request);
+    }
+
+    /// <summary>Constructs an AuthorizationMiddleware.</summary>
+    /// <param name="next">The next Ice.Object in the dispatch pipeline.</param>
+    /// <param name="validToken">The value to check against.</param>
+    internal AuthorizationMiddleware(Ice.Object next, string validToken)
+    {
+        _next = next;
+        _validToken = validToken;
+    }
+}
