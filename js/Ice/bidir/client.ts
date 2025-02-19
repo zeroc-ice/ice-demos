e9b5f1a695bc57be3ace679133bc5c2315524dd0
Jose <jose@zeroc.com>
2025-02-19 19:11:02 +0100

    Review fixes

--- js/Ice/bidir/client.ts
+++ js/Ice/bidir/client.ts
@@ -3,6 +3,8 @@
 import { Ice } from "@zeroc/ice";
 import { EarlyRiser } from "./AlarmClock.js";
 import { MockAlarmClock } from "./MockAlarmClock.js";
+import { toTicks } from "./timeUtil.js";
+import process from "node:process";
 
 // Create an Ice communicator to initialize the Ice runtime. The communicator is disposed before the program exits.
 await using communicator = Ice.initialize(process.argv);
@@ -22,19 +24,9 @@ const wakeUpService = new EarlyRiser.WakeUpServicePrx(communicator, "wakeUpServi
 
 // Schedule a wake-up call in 5 seconds. This call establishes the connection to the server; incoming requests over this
 // connection are handled by the communicator's default object adapter.
-
 await wakeUpService.wakeMeUp(toTicks(Date.now() + 5000));
 console.log("Wake-up call scheduled, falling asleep...");
 
 // Wait until the "stop" button is pressed on the mock alarm clock.
 await mockAlarmClock.StopPressed;
 console.log("Stop button pressed, exiting...");
-
-const millisecondsBeforeEpoch = 719162n * 24n * 60n * 60n * 1000n; // daysBeforeEpoch converted to milliseconds
-const ticksPerMillisecond = 10000n; // number of ticks per millisecond
-
-// Convert a timestamp representing the number of milliseconds elapsed since the epoch to a BigInt representing
-// the number of ticks (100 nanoseconds) since January 1, 0001 00:00:00 UTC in the Gregorian calendar.
-function toTicks(timestamp: number) {
-    return (BigInt(timestamp) + millisecondsBeforeEpoch) * ticksPerMillisecond;
-}
