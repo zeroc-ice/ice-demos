39314ffb5cbf90df01ffe45ec3ecddf14d8e7492
Jose <jose@zeroc.com>
2025-02-22 23:24:24 +0100

    Python Greeter demo improvements

--- python/Ice/greeter/server_amd/main.py
+++ python/Ice/greeter/server_amd/main.py
@@ -4,29 +4,26 @@
 import Ice
 import asyncio
 import chatbot
-import signal
 import sys
 
 async def main():
-    # Create an event to wait for Ctrl+C signal
-    exit_event = asyncio.Event()
-
-    loop = asyncio.get_running_loop()
-    loop.add_signal_handler(signal.SIGINT, lambda: exit_event.set())
-
     # Create an Ice communicator to initialize the Ice runtime. The communicator is disposed before the program exits.
     with Ice.initialize(sys.argv) as communicator:
         # Create an object adapter that listens for incoming requests and dispatches them to servants.
         adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061")
 
-        adapter.add(chatbot.Chatbot(loop), Ice.Util.stringToIdentity("greeter"))
+        # Register the Chatbot servant with the adapter.
+        adapter.add(chatbot.Chatbot(asyncio.get_running_loop()), Ice.Util.stringToIdentity("greeter"))
 
         # Start dispatching requests.
         adapter.activate()
         print("Listening on port 4061...")
 
-        # Wait until exit_event is set (e.g., via Ctrl+C)
-        await exit_event.wait()
-        print("Caught Ctrl+C, exiting...")
+        # Create an asyncio event to wait forever.
+        try:
+            await asyncio.Event().wait()
+        except asyncio.exceptions.CancelledError:
+            print("Caught Ctrl+C, exiting")
 
+# Craete the asyncio event loop and run the main couroutine on it.
 asyncio.run(main())
