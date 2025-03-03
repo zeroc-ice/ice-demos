aa6aa411c857c62516453909f1952604376f21bb
Bernard Normier <bernard@zeroc.com>
2025-03-03 17:18:13 -0500

    Update java/Ice/greeter shutdown

--- java/Ice/greeter/server/src/main/java/com/zeroc/demos/greeter/Server.java
+++ java/Ice/greeter/server/src/main/java/com/zeroc/demos/greeter/Server.java
@@ -2,17 +2,18 @@
 
 package com.zeroc.demos.greeter;
 
-import com.zeroc.demos.greeter.Chatbot;
 import com.zeroc.Ice.Communicator;
 import com.zeroc.Ice.ObjectAdapter;
 
 class Server {
     public static void main(String[] args) {
-        // Create an Ice communicator to initialize the Ice runtime. The communicator is disposed before the program exits.
+        // Create an Ice communicator to initialize the Ice runtime. The communicator is closed (destroyed) at the end
+        // of the try statement.
         try (Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                communicator.destroy();
-            }));
+
+            // Register a shutdown hook that calls communicator.shutdown() when the user shuts down the server with
+            // Ctrl+C or similar. The shutdown hook thread also waits until the current thread completes its cleanup.
+            shutdownCommunicatorOnCtrlC(communicator, Thread.currentThread());
 
             // Create an object adapter that listens for incoming requests and dispatches them to servants.
             ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061");
@@ -24,9 +25,22 @@ class Server {
             adapter.activate();
             System.out.println("Listening on port 4061...");
 
-            // Wait until the user presses Ctrl+C.
+            // Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
             communicator.waitForShutdown();
-            System.out.println("Caught Ctrl+C, exiting...");
         }
+    }
+
+    private static void shutdownCommunicatorOnCtrlC(Communicator communicator, Thread cleanupThread) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Caught Ctrl+C, shutting down...");
+            communicator.shutdown();
+
+            // Wait until the cleanup thread completes.
+            try {
+                cleanupThread.join();
+            } catch (InterruptedException e) {
+                assert false : "Shutdown thread cannot be interrupted";
+            }
+        }));
     }
 }
