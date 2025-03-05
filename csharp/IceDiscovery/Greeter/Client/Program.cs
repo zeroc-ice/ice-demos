8e0ad75b34071cdf1556be8590b701a4910dd6d6
Bernard Normier <bernard@zeroc.com>
2025-03-05 10:45:31 -0500

    Fix review comments

--- csharp/IceDiscovery/Greeter/Client/Program.cs
+++ csharp/IceDiscovery/Greeter/Client/Program.cs
@@ -3,7 +3,8 @@
 // Slice module VisitorCenter in Greeter.ice maps to C# namespace VisitorCenter.
 using VisitorCenter;
 
-// TBD
+// Configure the communicator to load the IceDiscovery plugin during initialization. This plugin installs a default
+// locator on the communicator.
 var initData = new Ice.InitializationData
 {
     properties = new Ice.Properties(ref args)
@@ -14,7 +15,8 @@ initData.properties.setProperty("Ice.Plugin.Discovery", "IceDiscovery:IceDiscove
 using Ice.Communicator communicator = Ice.Util.initialize(initData);
 
 // Create proxy to the Greeter object hosted by the server. "greeter" is a stringified proxy with no addressing
-// information, also known as a well-known proxy. It's resolved by the IceDiscovery plugin.
+// information, also known as a well-known proxy. It's resolved by the default locator installed by the IceDiscovery
+// plugin.
 GreeterPrx greeter = GreeterPrxHelper.createProxy(communicator, "greeter");
 
 // Send a request to the remote object and get the response.
