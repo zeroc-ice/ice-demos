72b6825401365f33b07b9a63648ebb6da53f13a3
Bernard Normier <bernard@zeroc.com>
2025-03-06 16:07:02 -0500

    New IceStorm Weather demo

---
+++ csharp/IceStorm/Weather/README.md
@@ -0,0 +1,38 @@
+#  IceStorm Weather
+
+This demo shows how to use IceStorm to create a simple pub-sub application. In this demo, one or more weather sensors
+report the local temperature and humidity to one or more weather stations via IceStorm. The sensors are the publishers
+while the weather stations are the subscribers.
+
+```mermaid
+flowchart LR
+    p1[Sensor #374] --report--> icestorm[IceStorm service<br>Hosts topic 'weather']
+    p2[Sensor #789] --report--> icestorm
+    icestorm --report--> s1[Station #1]
+    icestorm --report--> s2[Station #2]
+    icestorm --report--> s3[Station #3]
+```
+
+You can build the weather sensor and weather station applications with:
+
+``` shell
+dotnet build
+```
+
+Next, run the IceStorm service in its own terminal:
+
+```shell
+icebox --IceBox.Service.IceStorm="IceStormService,38a0:createIceStorm --Ice.Config=config.icestorm"
+```
+
+Then, run one or more sensors and weather stations, each in its own terminal. You can start them in any order.
+
+```shell
+cd Sensor
+dotnet run
+```
+
+```shell
+cd Station
+dotnet run
+```
---
+++ csharp/IceStorm/Weather/Sensor/Program.cs
@@ -0,0 +1,80 @@
+// Copyright (c) ZeroC, Inc.
+
+// Slice module ClearSky in WeatherStation.ice maps to C# namespace ClearSky.
+using ClearSky;
+using System.Diagnostics;
+
+// Create an Ice communicator to initialize the Ice runtime. The communicator is disposed before the program exits.
+using Ice.Communicator communicator = Ice.Util.initialize(ref args);
+
+// Create a proxy to the IceStorm topic manager.
+IceStorm.TopicManagerPrx topicManager = IceStorm.TopicManagerPrxHelper.createProxy(
+    communicator,
+    "ClearSky/TopicManager:tcp -p 4061");
+
+// Retrieve a proxy to the "weather" topic: we first create a topic with the given name (in case we are the first),
+// and then retrieve the proxy if the topic was already created.
+IceStorm.TopicPrx? topic;
+string topicName = "weather";
+try
+{
+    topic = await topicManager.createAsync(topicName);
+}
+catch (IceStorm.TopicExists)
+{
+    topic = await topicManager.retrieveAsync(topicName);
+}
+
+// The proxy returned by createAsync and retrieveAsync is never null.
+Debug.Assert(topic is not null);
+
+// Create a Weather station proxy using the publisher proxy of the topic. The proxy returned by getPublisher is
+// never null.
+WeatherStationPrx weatherStation = WeatherStationPrxHelper.uncheckedCast(topic.getPublisher()!);
+
+// The proxy returned by IceStorm is a two-way proxy. We can convert it into a oneway proxy if we don't need
+// acknowledgments from IceStorm.
+Debug.Assert(weatherStation.ice_isTwoway());
+
+var rand = new Random(); // pseudo-random number generator
+
+// Generate a sensor ID for this weather sensor.
+string sensorId = $"sensor-{rand.Next(1000)}";
+
+// Send a reading every seconds to the weather station(s) via IceStorm. We keep sending until the user presses Ctrl+C.
+Console.WriteLine($"{sensorId} reporting. Press Ctrl+C to stop...");
+
+using var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    cts.Cancel();
+};
+
+while (!cancellationToken.IsCancellationRequested)
+{
+    // Create a new AtmosphericConditions object with random values.
+    var reading = new AtmosphericConditions
+    {
+        Temperature = rand.Next(190, 230) / 10.0,
+        Humidity = rand.Next(450, 550) / 10.0,
+    };
+
+    try
+    {
+        // Send the reading to the weather station(s).
+        string timeStamp = DateTime.Now.ToString("T");
+        await weatherStation.ReportAsync(sensorId, timeStamp, reading, cancel: cancellationToken);
+
+        // Wait for one second before sending the next reading.
+        await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+    }
+    catch (OperationCanceledException)
+    {
+        // The user pressed Ctrl+C, we exit the while loop.
+        break;
+    }
+}
+
+Console.WriteLine("Exiting...");
