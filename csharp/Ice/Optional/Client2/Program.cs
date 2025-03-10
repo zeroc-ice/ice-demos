3beed1f33dfa7f949152bd5d864f1e93115ef1c9
Bernard Normier <bernard@zeroc.com>
2025-03-10 13:29:52 -0400

    Replace Random by RandomNumberGenerator (#351)

--- csharp/Ice/Optional/Client2/Program.cs
+++ csharp/Ice/Optional/Client2/Program.cs
@@ -2,6 +2,7 @@
 
 // Slice module ClearSky in WeatherStation2.ice maps to C# namespace ClearSky.
 using ClearSky;
+using System.Security.Cryptography; // for RandomNumberGenerator
 
 // Create an Ice communicator to initialize the Ice runtime. The communicator is disposed before the program exits.
 using Ice.Communicator communicator = Ice.Util.initialize(ref args);
@@ -10,12 +11,11 @@ using Ice.Communicator communicator = Ice.Util.initialize(ref args);
 WeatherStationPrx weatherStation = WeatherStationPrxHelper.createProxy(communicator, "weatherStation:tcp -p 4061");
 
 // Create an AtmosphericConditions object with random values.
-var rand = new Random();
 var reading = new AtmosphericConditions
 {
-    Temperature = rand.Next(190, 230) / 10.0,
-    Humidity = rand.Next(450, 550) / 10.0,
-    Pressure = rand.Next(10_000, 10_500) / 10.0,
+    Temperature = RandomNumberGenerator.GetInt32(190, 230) / 10.0,
+    Humidity = RandomNumberGenerator.GetInt32(450, 550) / 10.0,
+    Pressure = RandomNumberGenerator.GetInt32(10_000, 10_500) / 10.0,
 };
 
 // Report this reading to the weather station.
