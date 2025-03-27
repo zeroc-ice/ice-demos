9179016ed8ba9d19ad9838d859e3d884a194de5a
Jose <jose@zeroc.com>
2025-03-27 15:58:49 +0100

    Update Ice Greeter Python demo to use the event loop adapter

--- python/Ice/greeter/server_asyncio/main.py
+++ python/Ice/greeter/server_asyncio/main.py
@@ -2,29 +2,23 @@
 # Copyright (c) ZeroC, Inc.
 
 import Ice
-import chatbot
 import asyncio
+import chatbot
 import signal
 
 async def main():
 
-    loop = asyncio.get_event_loop()
+    # Get the current event loop.
+    loop=asyncio.get_running_loop()
 
-    def coroutineExecutor(coroutine):
-        """
-        A custom coroutine executor that runs the coroutine in the main asyncio event loop.
-        """
-
-        # We use run_coroutine_threadsafe because the executor is called by an Ice thread.
-        return asyncio.run_coroutine_threadsafe(coroutine, loop)
-
-    # Create an initialization data structure that sets the coroutine executor. The coroutine executor is used by Ice
-    # to run coroutines returned by servant dispatch methods in the main asyncio event loop.
-    initData = Ice.InitializationData()
-    initData.coroutineExecutor = coroutineExecutor
-
-    # Create an Ice communicator to initialize the Ice runtime. The communicator is destroyed at the end of the with block.
-    with Ice.initialize(initData) as communicator:
+    # Create an Ice communicator to initialize the Ice runtime. It will be automatically destroyed at the end of the
+    # with block.
+    #
+    # The event loop is passed to Ice.initialize so that the communicator is configured to use the
+    # Ice.asyncio.EventLoopAdapter, ensuring that:
+    # - Futures returned by the Ice runtime can be awaited within the asyncio event loop.
+    # - Coroutines from asynchronous dispatches run within the same event loop.
+    with Ice.initialize(eventLoop=loop) as communicator:
 
         # Shutdown the communicator when the user presses Ctrl+C.
         loop.add_signal_handler(signal.SIGINT, communicator.shutdown)
@@ -40,9 +34,6 @@ async def main():
         print("Listening on port 4061...")
 
         # Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
-        #
-        # Ice.wrap_future converts the Ice future returned by communicator.shutdownCompleted()
-        # into an asyncio future, making it awaitable within the asyncio event loop.
-        await Ice.wrap_future(communicator.shutdownCompleted())
+        await communicator.shutdownCompleted()
 
 asyncio.run(main())
