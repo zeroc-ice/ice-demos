5296bac5b6ed60c688a773e40dbe42d18894fa48
Jose <pepone@users.noreply.github.com>
2025-03-31 17:41:11 +0200

    Add Ice/config demo (#367)

--- python/Ice/config/server/main.py
+++ python/Ice/config/server/main.py
@@ -6,17 +6,21 @@ import chatbot
 import sys
 
 def main():
-    # Create an Ice communicator to initialize the Ice runtime. The communicator is destroyed at the end of the with block.
-    with Ice.initialize(sys.argv) as communicator:
+    # Create an Ice communicator to initialize the Ice runtime. The communicator is destroyed at the end of the with
+    # block. The communicator gets its configuration properties from file config.server, in the server's current
+    # working directory. The communicator initialization also parses the command-line options to find and set
+    # additional properties.
+    with Ice.initialize(sys.argv, configFile="config.server") as communicator:
         # Create an object adapter that listens for incoming requests and dispatches them to servants.
-        adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061")
+        adapter = communicator.createObjectAdapter("GreeterAdapter")
 
         # Register the Chatbot servant with the adapter.
         adapter.add(chatbot.Chatbot(), Ice.stringToIdentity("greeter"))
 
         # Start dispatching requests.
         adapter.activate()
-        print("Listening on port 4061...")
+        
+        # Ice.Trace.Network=1 or greater shows on which interface(s) and port(s) the server is listening.
 
         try:
             # Wait until communicator.shutdown() is called, which never occurs in this demo.
