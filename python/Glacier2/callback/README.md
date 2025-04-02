cebd07887461093ca664a05ad880e97f3bd2cbdd
Jose <jose@zeroc.com>
2025-04-02 20:51:55 +0200

    Glacier2 callback demo

---
+++ python/Glacier2/callback/README.md
@@ -0,0 +1,77 @@
+# Ice Callback
+
+The Callback demo illustrates how to implement callbacks in a client application.
+
+In this demo, the client hosts an alarm clock (an Ice object), and asks the server's wake up service to call this
+object at a later time. The server opens a TCP connection to the client when making this call.
+
+```mermaid
+flowchart LR
+    c[Client<br>hosts AlarmClock] --connection1--> s[Server:4061<br>hosts WakeUpService]
+    s --connection2--> c
+```
+
+We recommend running each program in a separate Python virtual environment. If you are new to Python virtual
+environments, see [Python Virtual Environments].
+
+## Running the server
+
+Navigate to the `server` directory, to run the server program.
+
+- Create and activate a Python virtual environment:
+
+    ```shell
+    python -m venv venv
+    source venv/bin/activate  # On macOS/Linux
+    venv\Scripts\activate     # On Windows
+    ```
+
+- Install the program dependencies:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+- Compile the AlarmClock.ice file with the Slice compiler for Python:
+
+    ```shell
+    slice2py ../slice/AlarmClock.ice
+    ```
+
+- Run the server program:
+
+    ```shell
+    python main.py
+    ```
+
+## Running the client
+
+In a separate terminal, navigate to the `client` directory to run the client program.
+
+- Create and activate a Python virtual environment:
+
+    ```shell
+    python -m venv venv
+    source venv/bin/activate  # On macOS/Linux
+    venv\Scripts\activate     # On Windows
+    ```
+
+- Install the program dependencies:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+- Compile the AlarmClock.ice file with the Slice compiler for Python:
+
+    ```shell
+    slice2py ../slice/AlarmClock.ice
+    ```
+
+- Run the client program:
+
+    ```shell
+    python main.py
+    ```
+
+[Python Virtual Environments]: https://docs.python.org/3/tutorial/venv.html
---
+++ python/Glacier2/callback/client/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright (c) ZeroC, Inc.
+
+import Ice
+import Glacier2
+import asyncio
+import sys
+from EarlyRiser import AlarmClockPrx, WakeUpServicePrx
+from mock_alarm_clock import MockAlarmClock
+from common.time import toTimestamp
+from datetime import datetime, timedelta
+
+async def main():
+    loop = asyncio.get_running_loop()
+    # Create an Ice communicator. We'll use this communicator to create proxies, manage outgoing connections, and
+    # create an object adapter. We enable asyncio support by passing the current event loop to initialize.
+    with Ice.initialize(sys.argv, eventLoop=loop) as communicator:
+
+        # Create a proxy to the Glacier2 router. The addressing information (transport, host and port number) is derived from
+        # the value of Glacier2.Client.Endpoints in the glacier2 router configuration file.
+        router = Glacier2.RouterPrx(communicator, "Glacier2/router:tcp -h localhost -p 4063")
+
+        # Create a session with the Glacier2 router. In this demo, the Glacier2 router is configured to accept any
+        # username/password combination. This call establishes a network connection to the Glacier2 router; the lifetime of the
+        # session is the same as the lifetime of the connection.
+        session = await router.createSessionAsync(getpass.getuser(), "password")
+
+        # The proxy returned by createSession is None because we did not configure a SessionManager on the Glacier2 router.
+        assert session is None
+
+        # Obtain a category string from the router. We need to use this category for the identity of server->client callbacks
+        # invoked through the Glacier2 router.
+        clientCategory = await router.getCategoryForClientAsync();
+
+        # Create an object adapter with no name and no configuration, but with our router proxy. This object adapter is a
+        # "bidirectional" object adapter, like the one created by the Ice/Bidir client application. It does not listen on any
+        # port and it does not need to be activated.
+        adapter = communicator.createObjectAdapterWithRouter("", router);
+
+        # Register the MockAlarmClock servant with the adapter. The wake up service knows we use identity "alarmClock".
+        mockAlarmClock = MockAlarmClock(loop)
+        alarmClock = AlarmClockPrx.uncheckedCast(
+            adapter.add(mockAlarmClock, Identity(name="alarmClock", category=clientCategory)))
+
+        # Start dispatching requests.
+        adapter.activate()
+        print("Listening on ephemeral port...")
+
+        # Create a proxy to the wake-up service.
+        wakeUpService = WakeUpServicePrx(communicator, "wakeUpService:tcp -h localhost -p 4061")
+
+        # Configure the proxy to route requests using the Glacier2 router.
+        wakeUpService = wakeUpService.ice_router(router)
+
+        # Schedule a wake-up call in 5 seconds.
+        await wakeUpService.wakeMeUpAsync(alarmClock, toTimestamp(datetime.now() + timedelta(seconds=5)))
+        print("Wake-up call scheduled, falling asleep...")
+
+        # Wait until the "stop" button is pressed on the mock alarm clock.
+        await mockAlarmClock.waitForStopPressed()
+        print("Stop button pressed, exiting...")
+
+if __name__ == "__main__":
+    asyncio.run(main())
