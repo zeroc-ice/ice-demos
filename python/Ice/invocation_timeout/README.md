a0f1cca54dc441db74aefec07ed14bc3d765570e
Jose <jose@zeroc.com>
2025-04-02 12:08:06 +0200

    Rename cancellation to invocation_timeout

--- python/Ice/invocation_timeout/README.md
+++ python/Ice/invocation_timeout/README.md
@@ -1,7 +1,6 @@
-# Ice Cancellation
+# Ice Invocation Timeout
 
-The Cancellation demo shows how to cancel an asynchronous invocation using the future cancel method. It also shows a
-related feature: invocation timeouts
+The Invocation Timeout demo shows how to use invocation timeouts
 
 We recommend running each program in a separate Python virtual environment. If you are new to Python virtual
 environments, see [Python Virtual Environments].
--- python/Ice/invocation_timeout/server/main.py
+++ python/Ice/invocation_timeout/server/main.py
@@ -11,8 +11,8 @@ async def main():
     # Get the current event loop.
     loop=asyncio.get_running_loop()
 
-    # Create an Ice communicator to initialize the Ice runtime. It will be automatically destroyed at the end of the
-    # with block. We enable asyncio support by passing the current event loop to initialize.
+    # Create an Ice communicator. We'll use this communicator to create an object adapter. We enable asyncio support by
+    # passing the current event loop to initialize.
     with Ice.initialize(sys.argv, eventLoop=loop) as communicator:
         # Shutdown the communicator when the user presses Ctrl+C.
         loop.add_signal_handler(signal.SIGINT, communicator.shutdown)
