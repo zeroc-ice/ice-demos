5d0069e4e044902ffbc5e6f63d42287d0c404e5b
Jose <jose@zeroc.com>
2025-04-02 11:57:00 +0200

    Add Python optional demo
    Checkpoint

---
+++ python/Ice/optional/client2/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Copyright (c) ZeroC, Inc.
+
+import Ice
+import asyncio
+import random
+import sys
+from ClearSky import WeatherStationPrx, AtmosphericConditions
+
+async def main():
+    # Create an Ice communicator to initialize the Ice runtime. It will be automatically destroyed at the end of the
+    # with block. We enable asyncio support by passing the current event loop to initialize.
+    with Ice.initialize(sys.argv, eventLoop=asyncio.get_running_loop()) as communicator:
+
+        # Create a proxy to the Weather station.
+        weatherStation = WeatherStationPrx(communicator, "weatherStation:tcp -p 4061")
+
+        reading = AtmosphericConditions(
+            temperature = int(random.uniform(190, 230)) / 10.0,
+            humidity = int(random.uniform(450, 550)) / 10.0,
+            pressure = int(random.uniform(10_000, 10_500)) / 10.0)
+
+        # Report this reading to the weather station.
+        await weatherStation.reportAsync("sensor v2", reading)
+
+        print("sensor v2: sent reading to weather station")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ python/Ice/optional/server1/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright (c) ZeroC, Inc.
+
+import Ice
+from console_printer import ConsolePrinter
+import sys
+
+def main():
+    # Create an Ice communicator to initialize the Ice runtime. The communicator is destroyed at the end of the with block.
+    with Ice.initialize(sys.argv) as communicator:
+        # Create an object adapter that listens for incoming requests and dispatches them to servants.
+        adapter = communicator.createObjectAdapterWithEndpoints("GreeterAdapter", "tcp -p 4061")
+
+        # Register the Chatbot servant with the adapter.
+        adapter.add(ConsolePrinter(), Ice.stringToIdentity("weatherStation"))
+
+        # Start dispatching requests.
+        adapter.activate()
+        print("Listening on port 4061...")
+
+        try:
+            # Wait until communicator.shutdown() is called, which never occurs in this demo.
+            communicator.waitForShutdown()
+        except KeyboardInterrupt:
+            print("Caught Ctrl+C, exiting...")
+
+if __name__ == "__main__":
+    main()
