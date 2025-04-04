c1192a46bb7cb3f8f58615db839e6c4e624d770d
Jose <pepone@users.noreply.github.com>
2025-04-04 10:00:31 +0200

    Update Python async demos to use `async with` (#388)

--- python/Ice/context/client/main.py
+++ python/Ice/context/client/main.py
@@ -20,7 +20,7 @@ async def main():
     initData.eventLoopAdapter = Ice.asyncio.EventLoopAdapter(asyncio.get_running_loop())
 
     # Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections.
-    with Ice.initialize(initData=initData) as communicator:
+    async with Ice.initialize(initData=initData) as communicator:
 
         # Create a Greeter proxy. If you run the server on a different computer, replace localhost in the string below
         # with the server's hostname or IP address.
