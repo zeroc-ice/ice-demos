e8a7e2c234b175c9dbb9438a5af1370c526547c1
Jose <pepone@users.noreply.github.com>
2025-04-21 12:37:58 +0200

    Add C# Ice/Secure demo (#406)

---
+++ csharp/Ice/Secure/Server/Program.cs
@@ -0,0 +1,43 @@
+// Copyright (c) ZeroC, Inc.
+
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+// Create an Ice communicator. We'll use this communicator to create an object adapter.
+await using Ice.Communicator communicator = Ice.Util.initialize(ref args);
+
+// Create the authentication options using the test server certificate included with the demos.
+var sslAuthenticationOptions = new SslServerAuthenticationOptions
+{
+    ServerCertificateContext = SslStreamCertificateContext.Create(
+        X509CertificateLoader.LoadPkcs12FromFile(
+            "../../../../certs/server.p12",
+            password: "password",
+            keyStorageFlags: X509KeyStorageFlags.Exportable),
+        additionalCertificates: null)
+};
+
+// Create an object adapter that listens for incoming requests and dispatches them to servants. We specify an ssl
+// endpoint to use the SSL transport.
+Ice.ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints(
+    "GreeterAdapter",
+    "ssl -p 4061",
+    sslAuthenticationOptions);
+
+// Register the Chatbot servant with the adapter.
+adapter.add(new Server.Chatbot(), Ice.Util.stringToIdentity("greeter"));
+
+// Start dispatching requests.
+adapter.activate();
+Console.WriteLine("Listening on port 4063...");
+
+// Shut down the communicator when the user presses Ctrl+C.
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true; // don't terminate the process
+    Console.WriteLine("Caught Ctrl+C, shutting down...");
+    communicator.shutdown(); // starts shutting down
+};
+
+// Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
+await communicator.shutdownCompleted;
