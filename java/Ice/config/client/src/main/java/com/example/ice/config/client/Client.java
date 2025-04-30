7fde4d53b419e42c92107aef6bc25948aff1af0f
ayoitslilith <lilithplante@gmail.com>
2025-04-30 13:35:05 -0400

    Minor modifications

--- java/Ice/config/client/src/main/java/com/example/ice/config/client/Client.java
+++ java/Ice/config/client/src/main/java/com/example/ice/config/client/Client.java
@@ -5,12 +5,13 @@ package com.example.ice.config.client;
 import com.example.visitorcenter.GreeterPrx;
 import com.zeroc.Ice.Communicator;
 import com.zeroc.Ice.Util;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 class Client {
     public static void main(String[] args) {
-        // Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections.
+        // Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections. 
+        // The communicator gets its configuration properties from file config.client in the client's current working
+        // directory. The communicator initialization also parses the command-line options to find and set additiona
+        // properties.
         try (Communicator communicator = Util.initialize(args, "config.client")) {
             // We create a Greeter proxy using the value of the Greeter.Proxy property in config.client.
             GreeterPrx greeter = GreeterPrx.uncheckedCast(communicator.propertyToProxy("Greeter.Proxy"));
@@ -18,7 +19,6 @@ class Client {
             // Send a request to the remote object and get the response.
             String greeting = greeter.greet(System.getProperty("user.name"));
             System.out.println(greeting);
-
         }
     }
 }
