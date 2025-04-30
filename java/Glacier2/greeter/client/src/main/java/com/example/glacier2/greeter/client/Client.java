d5e5c3c6b5692a339e6c62b0f1192732095e4209
ayoitslilith <lilithplante@gmail.com>
2025-04-30 13:18:20 -0400

    C# parity

--- java/Glacier2/greeter/client/src/main/java/com/example/glacier2/greeter/client/Client.java
+++ java/Glacier2/greeter/client/src/main/java/com/example/glacier2/greeter/client/Client.java
@@ -17,12 +17,19 @@ class Client {
         // Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections.
         try (Communicator communicator = Util.initialize(args)) {
 
-            // Retrieve my username.
-            String username = System.getProperty("user.name");
-
             // Create a proxy to the Glacier2 router. The addressing information (transport, host and port number) is derived from
             // the value of Glacier2.Client.Endpoints in the glacier2 router configuration file.
             RouterPrx router = RouterPrx.createProxy(communicator, "Glacier2/router:tcp -h localhost -p 4063");
+
+            // Create a session with the Glacier2 router. In this demo, the Glacier2 router is configured to accept any
+            // username/password combination. This call establishes a network connection to the Glacier2 router; the lifetime of the
+            // session is the same as the lifetime of the connection.
+            SessionPrx session;
+            try {
+                session = router.createSession(System.getProperty("user.name"), "password");
+            } catch (PermissionDeniedException | CannotCreateSessionException e) {
+                System.out.println("Could not start session: " + e.getCause());
+            }
 
             // If you run the server on a different computer, replace localhost in the string below with the server's
             // hostname or IP address.
@@ -31,18 +38,8 @@ class Client {
             // Configure the proxy to route requests using the Glacier2 router.
             greeter = GreeterPrx.uncheckedCast(greeter.ice_router(router));
 
-            // Create a session with the Glacier2 router. In this demo, the Glacier2 router is configured to accept any
-            // username/password combination. This call establishes a network connection to the Glacier2 router; the lifetime of the
-            // session is the same as the lifetime of the connection.
-            SessionPrx session;
-            try {
-                session = router.createSession(username, "password");
-            } catch (PermissionDeniedException | CannotCreateSessionException e) {
-                System.out.println("Could not start session: " + e.getCause());
-            }
-
             // Send a request to the remote object and get the response with greetAsync.
-            String greeting = greeter.greet(username);
+            String greeting = greeter.greet(System.getProperty("user.name"));
             System.out.println(greeting);
         }
     }
