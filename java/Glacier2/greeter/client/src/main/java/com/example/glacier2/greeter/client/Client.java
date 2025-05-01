03c0729222de1799d19f45fb632f81c0a04126fd
ayoitslilith <lilithplante@gmail.com>
2025-05-01 11:32:55 -0400

    Assertion and exception catch modification

--- java/Glacier2/greeter/client/src/main/java/com/example/glacier2/greeter/client/Client.java
+++ java/Glacier2/greeter/client/src/main/java/com/example/glacier2/greeter/client/Client.java
@@ -16,19 +16,24 @@ class Client {
         // Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections.
         try (Communicator communicator = Util.initialize(args)) {
 
-            // Create a proxy to the Glacier2 router. The addressing information (transport, host and port number) is derived from
-            // the value of Glacier2.Client.Endpoints in the glacier2 router configuration file.
+            // Create a proxy to the Glacier2 router. The addressing information (transport, host and port number) is 
+            // derived from the value of Glacier2.Client.Endpoints in the glacier2 router configuration file.
             RouterPrx router = RouterPrx.createProxy(communicator, "Glacier2/router:tcp -h localhost -p 4063");
 
             // Create a session with the Glacier2 router. In this demo, the Glacier2 router is configured to accept any
-            // username/password combination. This call establishes a network connection to the Glacier2 router; the lifetime of the
-            // session is the same as the lifetime of the connection.
+            // username/password combination. This call establishes a network connection to the Glacier2 router; the 
+            // lifetime of the session is the same as the lifetime of the connection.
             SessionPrx session;
             try {
                 session = router.createSession(System.getProperty("user.name"), "password");
             } catch (PermissionDeniedException | CannotCreateSessionException e) {
-                System.out.println("Could not start session: " + e.getCause());
+                System.out.println("Could not create session: " + e.getCause());
+                return;
             }
+
+            // The proxy returned by createSession is null because we did not configure a SessionManager on the Glacier2
+            // router.
+            assert session == null;
 
             // If you run the server on a different computer, replace localhost in the string below with the server's
             // hostname or IP address.
