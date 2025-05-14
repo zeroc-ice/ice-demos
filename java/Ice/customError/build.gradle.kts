9efdac8cce472563a76021672fae9178b164d9ee
ayoitslilith <lilithplante@gmail.com>
2025-05-14 14:04:47 -0400

    customError Java Ice

---
+++ java/Ice/customError/build.gradle.kts
@@ -0,0 +1,21 @@
+// Copyright (c) ZeroC, Inc.
+
+plugins {
+    // Register the Slice Tools for Ice and Java plugin without applying it here.
+    // The plugin will be applied to all subprojects in the `subprojects` block below.
+    id("com.zeroc.ice.slice-tools") version "3.8.0-+" apply false
+}
+
+subprojects {
+    // Apply the application plugin to enable Java application support in subprojects.
+    apply(plugin = "application")
+
+    // Apply the Gradle Slice Tools for Ice and Java plugin to enable Slice compilation in all subprojects.
+    apply(plugin = "com.zeroc.ice.slice-tools")
+
+    repositories {
+        // Add the ZeroC Nightly repository for downloading Ice runtime and development artifacts.
+        maven("https://download.zeroc.com/nexus/repository/maven-nightly/")
+        mavenCentral()
+    }
+}
