4485eeb9774496478ad6c6f92219c30e2a421475
Bernard Normier <bernard@zeroc.com>
2025-05-23 17:25:32 -0400

    Add ServerAMD to greeter demo (Swift) (#470)

--- swift/Ice/greeter/README.md
+++ swift/Ice/greeter/README.md
@@ -2,19 +2,28 @@
 
 The Greeter demo illustrates how to send a request and wait for the response.
 
+It provides two implementations for the server: a synchronous dispatch implementation (in Server), and an asynchronous
+dispatch implementation (in ServerAMD). The same client works with both.
+
 You can build the client and server applications with:
 
 ```shell
 swift build
 ```
 
-You can start the Server program with:
+First start the Server program:
 
 ```shell
 swift run Server
 ```
 
-Then, in a separate terminal, start the Client program with:
+or
+
+```shell
+swift run ServerAMD
+```
+
+In a separate terminal, start the Client program:
 
 ```shell
 swift run Client
---
+++ swift/Ice/greeter/Sources/ServerAMD/main.swift
@@ -0,0 +1,36 @@
+// Copyright (c) ZeroC, Inc.
+
+import Ice
+
+// CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the beginning
+// of the program, before creating an Ice communicator or starting any thread.
+let ctrlCHandler = CtrlCHandler()
+
+// Create an Ice communicator. We'll use this communicator to create an object adapter.
+var args = CommandLine.arguments
+let communicator = try Ice.initialize(&args)
+
+// Destroy the communicator when the program exits.
+defer {
+    communicator.destroy()
+}
+
+// Create an object adapter that listens for incoming requests and dispatches them to servants.
+let adapter = try communicator.createObjectAdapterWithEndpoints(
+    name: "GreeterAdapter", endpoints: "tcp -p 4061")
+
+// Register the Chatbot servant with the adapter.
+try adapter.add(servant: GreeterDisp(Chatbot()), id: Ice.Identity(name: "greeter"))
+
+// Start dispatching requests.
+try adapter.activate()
+print("Listening on port 4061...")
+
+// Shutdown the communicator when the user presses Ctrl+C.
+ctrlCHandler.setCallback { signal in
+    print("Caught signal \(signal), shutting down...")
+    communicator.shutdown()
+}
+
+// Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
+await communicator.shutdownCompleted()
