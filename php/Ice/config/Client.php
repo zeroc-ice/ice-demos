72490b77f9658d8462ecde436be319fe01dff18d
Jose <jose@zeroc.com>
2025-06-03 15:21:17 +0200

    Add PHP Ice/config demo

---
+++ php/Ice/config/Client.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright (c) ZeroC, Inc.
+
+require_once 'Ice.php';
+require_once 'Greeter.php';
+
+// Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections. The
+// communicator gets its configuration properties from file config.client in the client's current working directory.
+// The communicator initialization also parses args to find and set additional properties.
+$initData = new Ice\InitializationData();
+$initData->properties = Ice\createProperties($argv);
+$initData->properties->load("config.client");
+$communicator = Ice\initialize($initData);
+
+// We create a Greeter proxy using the value of the Greeter.Proxy property in config.client.
+// It's null if the property is not set.
+$greeter = VisitorCenter\GreeterPrxHelper::uncheckedCast($communicator->propertyToProxy("Greeter.Proxy"));
+
+// Send a request to the remote object and get the response.
+$greeting = $greeter->greet(get_current_user());
+
+echo "$greeting\n";
+?>
