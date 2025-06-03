b0315a968411f49ec89a073fb8511b31f5bd4aab
Austin Henriksen <austin@zeroc.com>
2025-06-03 10:21:49 -0400

    Remove unnecessary Slice plugin from forwardingserver.

--- java/Ice/forwarder/forwardingserver/build.gradle.kts
+++ java/Ice/forwarder/forwardingserver/build.gradle.kts
@@ -3,9 +3,6 @@
 plugins {
     // Apply the application plugin to tell gradle this is a runnable Java application.
     id("application")
-
-    // Apply the Slice-tools plugin to enable Slice compilation.
-    id("com.zeroc.ice.slice-tools") version "3.8.+"
 }
 
 dependencies {
