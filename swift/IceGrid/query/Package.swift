d4248d1b50b1c2ea057d688e95a9c876f0b668a0
Bernard Normier <bernard@zeroc.com>
2025-06-06 19:35:50 -0400

    Update Swift and macos versions

--- swift/IceGrid/query/Package.swift
+++ swift/IceGrid/query/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 
 import Foundation
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "query",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v15)
     ],
     dependencies: [.package(url: "https://download.zeroc.com/nexus/repository/nightly/ice-swift.git", branch: "main")],
     targets: [
