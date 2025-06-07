c65514c214e882338c54d9a9a0874328119a2e77
Bernard Normier <bernard@zeroc.com>
2025-06-07 11:09:15 -0400

    Upgrade Package.swift of Swift demos (#484)

--- swift/Ice/filesystem/Sources/Server/MNode.swift
+++ swift/Ice/filesystem/Sources/Server/MNode.swift
@@ -19,7 +19,7 @@ class MNode: Node {
     // Since we want to reuse this servant class (MNode) in derived servant classes, we need to define an overridable
     // `dispatch` method. The default `dispatch` method provided by the generated Node protocol extension is not
     // overridable. See https://github.com/swiftlang/swift/issues/42725.
-    func dispatch(_ request: Ice.IncomingRequest) async throws -> Ice.OutgoingResponse {
+    func dispatch(_ request: sending Ice.IncomingRequest) async throws -> Ice.OutgoingResponse {
         // We implement dispatch by reusing the implementation provided by the generated Node protocol extension.
         try await Self.dispatch(self, request: request)
     }
