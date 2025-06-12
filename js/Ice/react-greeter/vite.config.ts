8f8924e560a85cb6dd444c3c6f59b90a27e7bc2f
Jose <jose@zeroc.com>
2025-06-12 12:16:35 +0200

    Cleanup

--- js/Ice/react-greeter/vite.config.ts
+++ js/Ice/react-greeter/vite.config.ts
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    define: {
-        global: "globalThis",
-    },
-    resolve: {
-        alias: {
-            process: "process/browser",
-            buffer: "buffer",
-            crypto: "crypto-browserify",
-            stream: "stream-browserify",
-            util: "util",
-        },
-    },
 });
