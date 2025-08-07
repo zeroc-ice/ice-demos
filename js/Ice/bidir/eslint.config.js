e49876917ea8db4f7feb6ef185f7d002f5effe6a
Jose <jose@zeroc.com>
2025-08-07 21:27:46 +0200

    Missing files

---
+++ js/Ice/bidir/eslint.config.js
@@ -0,0 +1,41 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        linterOptions: {
+            reportUnusedDisableDirectives: "off",
+        },
+        languageOptions: {
+            globals: { ...globals.node},
+        },
+    },
+    {
+        ignores: ["*.js"]
+    },
+    {
+        files: ["**/*.d.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
+);
