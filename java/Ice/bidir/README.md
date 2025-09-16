1d53302cdbea31364f3cc097b25ae884981f38ed
Jose <pepone@users.noreply.github.com>
2025-09-16 18:48:18 +0200

    Add Ice/bidir Java demo (#577)

---
+++ java/Ice/bidir/README.md
@@ -0,0 +1,39 @@
+# Ice Bidir
+
+The Bidir demo illustrates how to send requests "the other way around", from a server to a client, by reusing the
+connection established by the client to the server.
+
+```mermaid
+flowchart LR
+    c[Client<br>hosts AlarmClock] --bidir connection--> s[Server:4061<br>hosts WakeUpService]
+```
+
+This is particularly useful when the client application is behind a firewall that does not allow incoming connections.
+
+## Building the Project
+
+This project uses Gradle and the [Application plugin]. To build the client and server applications, run:
+
+```shell
+./gradlew build
+```
+
+This will compile each project and install the distributions in the `build/install` subdirectory of each project.
+
+## Running the Server
+
+First, start the server application:
+
+```shell
+./gradlew :server:run --quiet
+```
+
+## Running the Client
+
+Then, in a separate terminal, start the client application:
+
+```shell
+./gradlew :client:run --quiet
+```
+
+[Application plugin]: https://docs.gradle.org/current/userguide/application_plugin.html
---
+++ java/Ice/bidir/client/src/main/java/com/example/ice/bidir/client/Client.java
@@ -0,0 +1,51 @@
+// Copyright (c) ZeroC, Inc.
+
+package com.example.ice.bidir.client;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.concurrent.ExecutionException;
+
+import com.example.earlyriser.WakeUpServicePrx;
+import com.example.ice.bidir.util.Time;
+import com.zeroc.Ice.Communicator;
+import com.zeroc.Ice.Identity;
+import com.zeroc.Ice.ObjectAdapter;
+import com.zeroc.Ice.Util;
+
+class Client {
+    public static void main(String[] args) {
+        // Create an Ice communicator. We'll use this communicator to create proxies, manage outgoing connections, and
+        // create an object adapter.
+        try (Communicator communicator = Util.initialize(args)) {
+            // Create an object adapter with no name and no configuration. This object adapter does not need to be
+            // activated.
+            ObjectAdapter adapter = communicator.createObjectAdapter("");
+
+            // Sets this object adapter as the default object adapter on the communicator.
+            communicator.setDefaultObjectAdapter(adapter);
+
+            // Register the MockAlarmClock servant with the adapter. The wake up service knows we use identity
+            // "alarmClock".
+            var mockAlarmClock = new MockAlarmClock();
+            adapter.add(mockAlarmClock, new Identity("alarmClock", ""));
+
+            // Create a proxy to the wake-up service.
+            WakeUpServicePrx wakeUpService = WakeUpServicePrx.createProxy(
+                communicator,
+                "wakeUpService:tcp -h localhost -p 4061");
+
+            // Schedule a wake-up call in 5 seconds. This call establishes the connection to the server; incoming
+            // requests over this connection are handled by the communicator's default object adapter.
+            wakeUpService.wakeMeUp(Time.toTimeStamp(ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(5)));
+            System.out.println("Wake-up call scheduled, falling asleep...");
+            try {
+                // Wait until the user presses the stop button on the alarm clock.
+                mockAlarmClock.stopPressed().get();
+                System.out.println("Stop button pressed, exiting...");
+            } catch (InterruptedException | ExecutionException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+}
