45d061df5ba6296c6b56fffca4c4c5a4f1bc20db
Jose <pepone@users.noreply.github.com>
2025-09-23 17:33:44 +0200

    Add JavaScript prettier workflow (#605)

--- js/Ice/bidir/eslint.config.js
+++ js/Ice/bidir/eslint.config.js
@@ -10,11 +10,11 @@ export default tseslint.config(
             reportUnusedDisableDirectives: "off",
         },
         languageOptions: {
-            globals: { ...globals.node},
+            globals: { ...globals.node },
         },
     },
     {
-        ignores: ["*.js"]
+        ignores: ["*.js"],
     },
     {
         files: ["**/*.d.ts"],
