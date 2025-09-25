497b1cd37feca9885d4d6131fd01d48475b36df5
Bernard Normier <bernard@zeroc.com>
2025-09-25 14:55:50 -0400

    Fix calls to initialize in PHP (#614)

--- php/IceDiscovery/greeter/Client.php
+++ php/IceDiscovery/greeter/Client.php
@@ -7,12 +7,12 @@ require_once 'Greeter.php';
 // Configure the communicator to load the IceDiscovery plug-in during initialization. This plug-in installs a default
 // locator on the communicator.
 $initData = new Ice\InitializationData();
-$initData->properties = Ice\createProperties();
+$initData->properties = Ice\createProperties($argv);
 $initData->properties->setProperty('Ice.Plugin.IceDiscovery', '1');
 
 // Create an Ice communicator. We'll use this communicator to create proxies and manage outgoing connections.
 // This communicator is destroyed automatically at the end of the script.
-$communicator = Ice\initialize($argv, $initData);
+$communicator = Ice\initialize($initData);
 
 // Create a proxy to the Greeter object hosted by the server(s). 'greeter' is a stringified proxy with no addressing
 // information, also known as a well-known proxy. It's resolved by the default locator installed by the IceDiscovery
