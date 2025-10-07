ec47cd04c665e9f3fa92abd8c085b28754c19689
Jose <jose@zeroc.com>
2025-10-07 18:41:54 +0200

    cleanup

--- python/IceDiscovery/replication/server/main.py
+++ python/IceDiscovery/replication/server/main.py
@@ -9,15 +9,17 @@ import Ice
 
 
 def main():
-    # Configure the communicator to load the IceDiscovery plug-in during initialization. This plug-in installs a default
-    # locator on the communicator.
     initData = Ice.InitializationData()
     initData.properties = Ice.createProperties(sys.argv)
+
+    # Configure the communicator to load the IceDiscovery plug-in during initialization. This plug-in installs a default
+    # locator on the communicator.
     initData.properties.setProperty("Ice.Plugin.IceDiscovery", "1")
 
-    greeter_uuid = str(uuid.uuid4())
     # Configure the object adapter GreeterAdapter. It must be an indirect object adapter (i.e., with an AdapterId
     # property); otherwise, the IceDiscovery plug-in can't make it discoverable by IceDiscovery clients.
+    # We also set the ReplicaGroupId property to "greeterPool" to enable replication.
+    greeter_uuid = str(uuid.uuid4())
     initData.properties.setProperty("GreeterAdapter.AdapterId", f"greeter-{greeter_uuid}")
     initData.properties.setProperty("GreeterAdapter.ReplicaGroupId", "greeterPool")
 
@@ -30,7 +32,8 @@ def main():
         # Create an object adapter that listens for incoming requests and dispatches them to servants.
         adapter = communicator.createObjectAdapter("GreeterAdapter")
 
-        # Register the Chatbot servant with the adapter.
+        # Register the Chatbot servant with the adapter. Here, well-known object "greeter" is replicated across all
+        # server instances.
         adapter.add(chatbot.Chatbot(greeter_uuid[0:4]), Ice.Identity(name="greeter"))
 
         # Start dispatching requests. This method also registers the object adapter with the IceDiscovery plug-in.
