0782ed54404cb2c590593d766e5cf5bad08347c3
Austin Henriksen <austin.r.henriksen79@gmail.com>
2025-10-20 10:36:51 -0400

    Extra 's' which shouldn't be there.

--- java/Glacier2/session/settings.gradle.kts
+++ java/Glacier2/session/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         gradlePluginPortal()
         // Use the nightly build of the Slice Tools plugin from the ZeroC maven-nightly repository.
         maven {
-            url = uri("https://download.zeroc.com/nexus/repository/maven-nightly/s")
+            url = uri("https://download.zeroc.com/nexus/repository/maven-nightly/")
             content {
                 includeGroupByRegex("com\\.zeroc.*")
             }
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
         mavenCentral()
         // Use the nightly build of Ice from the ZeroC maven-nightly repository.
         maven {
-            url = uri("https://download.zeroc.com/nexus/repository/maven-nightly/s")
+            url = uri("https://download.zeroc.com/nexus/repository/maven-nightly/")
             content {
                 includeGroupByRegex("com\\.zeroc.*")
             }
