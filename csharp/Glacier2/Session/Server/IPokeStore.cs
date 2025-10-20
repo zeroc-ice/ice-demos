eac41e40ae5177002326bca0f232f89ea873520a
Austin Henriksen <austin.r.henriksen79@gmail.com>
2025-10-20 10:18:17 -0400

    Not worth the nitpick honestly...

--- csharp/Glacier2/Session/Server/IPokeStore.cs
+++ csharp/Glacier2/Session/Server/IPokeStore.cs
@@ -5,12 +5,12 @@ namespace Server;
 /// <summary>Represents a Pokémon storage system.</summary>
 internal interface IPokeStore
 {
-    /// <summary>Saves the Pokémon collection of a specific user.</summary>
+    /// <summary>Saves the Pokémon collection for a specific user.</summary>
     /// <param name="userId">The user's ID.</param>
     /// <param name="pokemon">The Pokémon collection to save.</param>
     void SaveCollection(string userId, IReadOnlyCollection<string> pokemon);
 
-    /// <summary>Retrieves the Pokémon collection of a specific user.</summary>
+    /// <summary>Retrieves the Pokémon collection for a specific user.</summary>
     /// <param name="userId">The user's ID.</param>
     /// <returns>The saved Pokémon collection, or null if no collection was saved for <paramref name="userId"/>.
     /// </returns>
