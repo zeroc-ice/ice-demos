dc787ef6776d843a33f6b6f7d4a479d840d095d4
Jose <jose@zeroc.com>
2025-10-31 11:34:54 +0100

    Simplify DataStorm node demo

--- cpp/DataStorm/node/Reader.cpp
+++ cpp/DataStorm/node/Reader.cpp
@@ -10,53 +10,45 @@ using namespace std;
 int
 main(int argc, char* argv[])
 {
-    try
-    {
-        // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
-        // beginning of the program, before creating a DataStorm node or starting any thread.
-        Ice::CtrlCHandler ctrlCHandler;
+    // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
+    // beginning of the program, before creating a DataStorm node or starting any thread.
+    Ice::CtrlCHandler ctrlCHandler;
 
-        // Default properties.
-        auto defaultProperties = make_shared<Ice::Properties>();
-        // Connect to the DataStorm node running on localhost port 10000
-        defaultProperties->setProperty("DataStorm.Node.ConnectTo", "tcp -h localhost -p 10000");
-        // Disable the multicast endpoint
-        defaultProperties->setProperty("DataStorm.Node.Multicast.Enabled", "0");
+    // Default properties.
+    auto defaultProperties = make_shared<Ice::Properties>();
+    // Connect to the DataStorm node running on localhost port 4061.
+    defaultProperties->setProperty("DataStorm.Node.ConnectTo", "tcp -h localhost -p 4061");
+    // Disable the multicast endpoint
+    defaultProperties->setProperty("DataStorm.Node.Multicast.Enabled", "0");
 
-        Ice::InitializationData initData;
-        initData.properties = make_shared<Ice::Properties>(argc, argv, defaultProperties);
+    Ice::InitializationData initData;
+    initData.properties = make_shared<Ice::Properties>(argc, argv, defaultProperties);
 
-        // Create an Ice communicator. We'll use this communicator to instantiate the DataStorm node.
-        Ice::CommunicatorPtr communicator = Ice::initialize(initData);
+    // Create an Ice communicator. We'll use this communicator to instantiate the DataStorm node.
+    Ice::CommunicatorPtr communicator = Ice::initialize(initData);
 
-        // Make sure the communicator is destroyed at the end of this scope.
-        Ice::CommunicatorHolder communicatorHolder{communicator};
+    // Make sure the communicator is destroyed at the end of this scope.
+    Ice::CommunicatorHolder communicatorHolder{communicator};
 
-        // Instantiates node.
-        DataStorm::Node node{communicator};
+    // Instantiates node.
+    DataStorm::Node node{communicator};
 
-        // Shutdown the node on Ctrl-C.
-        ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
+    // Shutdown the node on Ctrl-C.
+    ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
 
-        // Instantiates the "time" topic.
-        DataStorm::Topic<int, string> topic{node, "time"};
+    // Instantiates the "time" topic.
+    DataStorm::Topic<int, string> topic{node, "time"};
 
-        // Instantiate a reader to read the time from all the topic writers.
-        auto reader = DataStorm::makeAnyKeyReader(topic);
+    // Instantiate a reader to read the time from all the topic writers.
+    auto reader = DataStorm::makeAnyKeyReader(topic);
 
-        // Prints out the received samples.
-        reader.onSamples(
-            nullptr,
-            [](const DataStorm::Sample<int, string>& sample)
-            { cout << "[" << sample.getKey() << "] received time: " << sample.getValue() << endl; });
+    // Prints out the received samples.
+    reader.onSamples(
+        nullptr,
+        [](const DataStorm::Sample<int, string>& sample)
+        { cout << "[" << sample.getKey() << "] received time: " << sample.getValue() << endl; });
 
-        // Exit once the user hits Ctrl-C to shutdown the node.
-        node.waitForShutdown();
-    }
-    catch (const std::exception& ex)
-    {
-        cerr << ex.what() << endl;
-        return 1;
-    }
+    // Exit once the user hits Ctrl-C to shutdown the node.
+    node.waitForShutdown();
     return 0;
 }
--- cpp/DataStorm/node/Writer.cpp
+++ cpp/DataStorm/node/Writer.cpp
@@ -13,57 +13,49 @@ using namespace std;
 int
 main(int argc, char* argv[])
 {
-    try
+    // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
+    // beginning of the program, before creating a DataStorm node or starting any thread.
+    Ice::CtrlCHandler ctrlCHandler;
+
+    // Default properties.
+    auto defaultProperties = make_shared<Ice::Properties>();
+    // Connect to the DataStorm node running on localhost port 4061
+    defaultProperties->setProperty("DataStorm.Node.ConnectTo", "tcp -h localhost -p 4061");
+    // Disable the multicast endpoint
+    defaultProperties->setProperty("DataStorm.Node.Multicast.Enabled", "0");
+
+    Ice::InitializationData initData;
+    initData.properties = make_shared<Ice::Properties>(argc, argv, defaultProperties);
+
+    // Create an Ice communicator. We'll use this communicator to instantiate the DataStorm node.
+    Ice::CommunicatorPtr communicator = Ice::initialize(initData);
+
+    // Make sure the communicator is destroyed at the end of this scope.
+    Ice::CommunicatorHolder communicatorHolder{communicator};
+
+    // Instantiates node.
+    DataStorm::Node node{communicator};
+
+    // Shutdown the node on Ctrl-C.
+    ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
+
+    // Instantiates the "time" topic.
+    DataStorm::Topic<int, string> topic{node, "time"};
+
+    // Setup a random generator to generate an identifier for the writer.
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<> id(1);
+
+    // Instantiate a writer to write the local time. The writer key is a random
+    // integer value that identifies the writer.
+    auto writer = DataStorm::makeSingleKeyWriter(topic, id(generator));
+
+    while (!node.isShutdown())
     {
-        // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
-        // beginning of the program, before creating a DataStorm node or starting any thread.
-        Ice::CtrlCHandler ctrlCHandler;
-
-        // Default properties.
-        auto defaultProperties = make_shared<Ice::Properties>();
-        // Connect to the DataStorm node running on localhost port 10000
-        defaultProperties->setProperty("DataStorm.Node.ConnectTo", "tcp -h localhost -p 10000");
-        // Disable the multicast endpoint
-        defaultProperties->setProperty("DataStorm.Node.Multicast.Enabled", "0");
-
-        Ice::InitializationData initData;
-        initData.properties = make_shared<Ice::Properties>(argc, argv, defaultProperties);
-
-        // Create an Ice communicator. We'll use this communicator to instantiate the DataStorm node.
-        Ice::CommunicatorPtr communicator = Ice::initialize(initData);
-
-        // Make sure the communicator is destroyed at the end of this scope.
-        Ice::CommunicatorHolder communicatorHolder{communicator};
-
-        // Instantiates node.
-        DataStorm::Node node{communicator};
-
-        // Shutdown the node on Ctrl-C.
-        ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
-
-        // Instantiates the "time" topic.
-        DataStorm::Topic<int, string> topic{node, "time"};
-
-        // Setup a random generator to generate an identifier for the writer.
-        random_device rd;
-        mt19937 generator(rd());
-        uniform_int_distribution<> id(1);
-
-        // Instantiate a writer to write the local time. The writer key is a random
-        // integer value that identifies the writer.
-        auto writer = DataStorm::makeSingleKeyWriter(topic, id(generator));
-
-        while (!node.isShutdown())
-        {
-            auto timeString = Time::formatTime(chrono::system_clock::now());
-            writer.update(timeString);
-            this_thread::sleep_for(chrono::seconds(1));
-        }
-    }
-    catch (const std::exception& ex)
-    {
-        cerr << ex.what() << endl;
-        return 1;
+        auto timeString = Time::formatTime(chrono::system_clock::now());
+        writer.update(timeString);
+        this_thread::sleep_for(chrono::seconds(1));
     }
     return 0;
 }
