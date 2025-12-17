9b05fcad20c7128a6f59f18dc2cbc354cd0e15d5
Bernard Normier <bernard@zeroc.com>
2025-12-17 17:22:07 -0500

    Remove redundant try/catch blocks

--- cpp/DataStorm/stock/Reader.cpp
+++ cpp/DataStorm/stock/Reader.cpp
@@ -15,131 +15,123 @@ using namespace Demo;
 int
 main(int argc, char* argv[])
 {
-    try
+    // Set a custom terminate handler to print unhandled exceptions to cerr.
+    std::set_terminate(Terminate::printCurrentException);
+
+    // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
+    // beginning of the program, before creating a DataStorm node or starting any thread.
+    Ice::CtrlCHandler ctrlCHandler;
+
+    // Instantiates node.
+    DataStorm::Node node{argc, argv};
+
+    // Shutdown the node on Ctrl-C.
+    ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
+
+    // Instantiates the "stocks" topic.
+    DataStorm::Topic<string, Demo::Stock> topic{node, "stocks"};
+
+    // Setup partial update updaters. The updater is responsible for updating the element value when a partial
+    // update is received. Updaters must be set on the topic from both the reader and writer.
+    topic.setUpdater<float>("price", [](Stock& stock, float price) { stock.price = price; });
+    topic.setUpdater<int>("volume", [](Stock& stock, int volume) { stock.volume = volume; });
+
+    shared_ptr<DataStorm::Topic<string, Demo::Stock>::ReaderType> reader;
     {
-        // Set a custom terminate handler to print unhandled exceptions to cerr.
-        std::set_terminate(Terminate::printCurrentException);
+        // Create a reader that connects to all the keys but doesn't receive any samples (we use the `_event'
+        // predefined sample filter with an empty set of sample events to discard events on the writer).
+        auto stocks = DataStorm::makeAnyKeyReader(
+            topic,
+            DataStorm::Filter<DataStorm::SampleEventSeq>("_event", DataStorm::SampleEventSeq{}));
+        stocks.waitForWriters();
 
-        // CtrlCHandler is a helper class that handles Ctrl+C and similar signals. It must be constructed at the
-        // beginning of the program, before creating a DataStorm node or starting any thread.
-        Ice::CtrlCHandler ctrlCHandler;
-
-        // Instantiates node.
-        DataStorm::Node node{argc, argv};
-
-        // Shutdown the node on Ctrl-C.
-        ctrlCHandler.setCallback([&node](int) { node.shutdown(); });
-
-        // Instantiates the "stocks" topic.
-        DataStorm::Topic<string, Demo::Stock> topic{node, "stocks"};
-
-        // Setup partial update updaters. The updater is responsible for updating the element value when a partial
-        // update is received. Updaters must be set on the topic from both the reader and writer.
-        topic.setUpdater<float>("price", [](Stock& stock, float price) { stock.price = price; });
-        topic.setUpdater<int>("volume", [](Stock& stock, int volume) { stock.volume = volume; });
-
-        shared_ptr<DataStorm::Topic<string, Demo::Stock>::ReaderType> reader;
-        {
-            // Create a reader that connects to all the keys but doesn't receive any samples (we use the `_event'
-            // predefined sample filter with an empty set of sample events to discard events on the writer).
-            auto stocks = DataStorm::makeAnyKeyReader(
-                topic,
-                DataStorm::Filter<DataStorm::SampleEventSeq>("_event", DataStorm::SampleEventSeq{}));
-            stocks.waitForWriters();
-
-            // Get the set of stocks connected with the any reader and display their ticker.
-            std::promise<void> p;
-            stocks.onConnectedKeys(
-                [&p](const vector<string>& tickers)
-                {
-                    cout << "Available stock(s): " << endl;
-                    for (const auto& ticker : tickers)
-                    {
-                        cout << ticker << endl;
-                    }
-                    p.set_value();
-                },
-                [](DataStorm::CallbackReason action, const string& ticker)
-                {
-                    if (action == DataStorm::CallbackReason::Connect)
-                    {
-                        cout << "New stock available: " << ticker << endl;
-                    }
-                    else
-                    {
-                        cout << "Stock no longer available: " << ticker << endl;
-                    }
-                });
-            p.get_future().get();
-
-            string stock;
-            cout << "Please enter the stock to follow (default = all):\n";
-            getline(cin, stock);
-
-            // Read values for the given stock using a key reader.
-            if (stock.empty() || stock == "all")
+        // Get the set of stocks connected with the any reader and display their ticker.
+        std::promise<void> p;
+        stocks.onConnectedKeys(
+            [&p](const vector<string>& tickers)
             {
-                reader = make_shared<DataStorm::MultiKeyReader<string, Stock>>(topic, vector<string>{});
-            }
-            else
-            {
-                auto tickers = stocks.getConnectedKeys();
-                if (ranges::find(tickers, stock) == tickers.end())
+                cout << "Available stock(s): " << endl;
+                for (const auto& ticker : tickers)
                 {
-                    cout << "unknown stock `" << stock << "'" << endl;
-                    return 1;
+                    cout << ticker << endl;
                 }
-                reader = make_shared<DataStorm::SingleKeyReader<string, Stock>>(topic, stock);
+                p.set_value();
+            },
+            [](DataStorm::CallbackReason action, const string& ticker)
+            {
+                if (action == DataStorm::CallbackReason::Connect)
+                {
+                    cout << "New stock available: " << ticker << endl;
+                }
+                else
+                {
+                    cout << "Stock no longer available: " << ticker << endl;
+                }
+            });
+        p.get_future().get();
+
+        string stock;
+        cout << "Please enter the stock to follow (default = all):\n";
+        getline(cin, stock);
+
+        // Read values for the given stock using a key reader.
+        if (stock.empty() || stock == "all")
+        {
+            reader = make_shared<DataStorm::MultiKeyReader<string, Stock>>(topic, vector<string>{});
+        }
+        else
+        {
+            auto tickers = stocks.getConnectedKeys();
+            if (ranges::find(tickers, stock) == tickers.end())
+            {
+                cout << "unknown stock `" << stock << "'" << endl;
+                return 1;
+            }
+            reader = make_shared<DataStorm::SingleKeyReader<string, Stock>>(topic, stock);
+        }
+    }
+
+    // Wait for the writer to be connected.
+    reader->waitForWriters();
+
+    auto displaySample = [](const DataStorm::Sample<string, Stock>& s)
+    {
+        if (s.getEvent() == DataStorm::SampleEvent::Add || s.getEvent() == DataStorm::SampleEvent::Update)
+        {
+            const auto& value = s.getValue();
+            cout << "Stock: " << value.name << " (" << s.getKey() << ")" << endl;
+            cout << "Price: " << value.price << endl;
+            cout << "Best bid/ask: " << value.bestBid << '/' << value.bestAsk << endl;
+            cout << "Market Cap: " << value.marketCap << endl;
+            cout << "Previous close: " << value.previousClose << endl;
+            cout << "Volume: " << value.volume << endl;
+            cout << endl;
+        }
+        else if (s.getEvent() == DataStorm::SampleEvent::PartialUpdate)
+        {
+            if (s.getUpdateTag() == "price")
+            {
+                cout << "received price update for " << s.getKey() << ": " << s.getValue().price << endl;
+            }
+            else if (s.getUpdateTag() == "volume")
+            {
+                cout << "received volume update for " << s.getKey() << ": " << s.getValue().volume << endl;
             }
         }
+    };
 
-        // Wait for the writer to be connected.
-        reader->waitForWriters();
-
-        auto displaySample = [](const DataStorm::Sample<string, Stock>& s)
+    // Print out the sample values queued with this reader.
+    reader->onSamples(
+        [displaySample](const vector<DataStorm::Sample<string, Stock>>& samples)
         {
-            if (s.getEvent() == DataStorm::SampleEvent::Add || s.getEvent() == DataStorm::SampleEvent::Update)
+            for (const auto& s : samples)
             {
-                const auto& value = s.getValue();
-                cout << "Stock: " << value.name << " (" << s.getKey() << ")" << endl;
-                cout << "Price: " << value.price << endl;
-                cout << "Best bid/ask: " << value.bestBid << '/' << value.bestAsk << endl;
-                cout << "Market Cap: " << value.marketCap << endl;
-                cout << "Previous close: " << value.previousClose << endl;
-                cout << "Volume: " << value.volume << endl;
-                cout << endl;
+                displaySample(s);
             }
-            else if (s.getEvent() == DataStorm::SampleEvent::PartialUpdate)
-            {
-                if (s.getUpdateTag() == "price")
-                {
-                    cout << "received price update for " << s.getKey() << ": " << s.getValue().price << endl;
-                }
-                else if (s.getUpdateTag() == "volume")
-                {
-                    cout << "received volume update for " << s.getKey() << ": " << s.getValue().volume << endl;
-                }
-            }
-        };
+        },
+        displaySample);
 
-        // Print out the sample values queued with this reader.
-        reader->onSamples(
-            [displaySample](const vector<DataStorm::Sample<string, Stock>>& samples)
-            {
-                for (const auto& s : samples)
-                {
-                    displaySample(s);
-                }
-            },
-            displaySample);
-
-        // Exit once the user hits Ctrl-C to shutdown the node.
-        node.waitForShutdown();
-    }
-    catch (const std::exception& ex)
-    {
-        cerr << ex.what() << endl;
-        return 1;
-    }
+    // Exit once the user hits Ctrl-C to shutdown the node.
+    node.waitForShutdown();
     return 0;
 }
