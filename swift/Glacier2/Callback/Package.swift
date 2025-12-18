13aec257b535ed086c247a406e2b69350c093168
Jose <jose@zeroc.com>
2025-12-18 16:11:48 +0100

    Update swift  dependencies to use 3.8 branch

--- swift/Glacier2/Callback/Package.swift
+++ swift/Glacier2/Callback/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v15)
     ],
     dependencies: [
-        .package(url: "https://github.com/zeroc-ice/ice.git", .upToNextMinor(from: "3.8.0")),
+        .package(url: "https://github.com/zeroc-ice/ice.git", branch: "3.8"),
         .package(name: "common", path: "../../common"),
     ],
     targets: [
