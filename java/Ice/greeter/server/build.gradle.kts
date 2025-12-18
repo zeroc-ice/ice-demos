1b53b16352cefa91fe931ed4fe28c215adc76c55
Bernard Normier <bernard@zeroc.com>
2025-12-18 10:41:59 -0500

    Remove setIncludes from build.gradle.kts (#695)

--- java/Ice/greeter/server/build.gradle.kts
+++ java/Ice/greeter/server/build.gradle.kts
@@ -21,9 +21,6 @@ sourceSets {
         // Add the Greeter.ice file from the parent slice directory to the main source set.
         slice {
             srcDirs("../slice")
-            // By default a Slice source set includes all Slice files in the srcDirs directories.
-            // Here we override the default behavior by specifying the list of Slice files to include.
-            setIncludes(listOf("Greeter.ice"))
         }
     }
 }
