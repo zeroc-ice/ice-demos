0084f5d9d952a5cc2f9a5ad39cde7cdf7e8499c2
Bernard Normier <bernard@zeroc.com>
2025-12-18 10:41:59 -0500

    Remove setIncludes from build.gradle.kts (#695)

--- java/IceStorm/weather/sensor/build.gradle.kts
+++ java/IceStorm/weather/sensor/build.gradle.kts
@@ -22,9 +22,6 @@ sourceSets {
         // Add the WeatherStation.ice file from the parent slice directory to the main source set.
         slice {
             srcDirs("../slice")
-            // By default a Slice source set includes all Slice files in the srcDirs directories.
-            // Here we override the default behavior by specifying the list of Slice files to include.
-            setIncludes(listOf("WeatherStation.ice"))
         }
     }
 }
