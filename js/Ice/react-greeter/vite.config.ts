6b4cdfb2ce92110a25a9c82b50800fc265a19a21
Jose <pepone@users.noreply.github.com>
2026-02-13 15:16:21 +0100

    Use slice2js unplugin with esbuild and Vite for JS demo builds (#737)

--- js/Ice/react-greeter/vite.config.ts
+++ js/Ice/react-greeter/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import slice2js from "@zeroc/slice2js/unplugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        slice2js({
+            inputs: ["src/Greeter.ice"],
+            args: ["--typescript"],
+            outputDir: "src",
+        }),
+    ],
 });
