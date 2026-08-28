a2380ea5b932882599f829d9893df6d514b24f92
Joe George <joe@zeroc.com>
2026-08-28 11:57:55 -0400

    Update the java demos to Gradle 9.7.1 and current dependencies (#913)

--- java/Ice/android-greeter/app/build.gradle.kts
+++ java/Ice/android-greeter/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.ice.androidgreeter"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.ice.androidgreeter"
--- java/build-logic/build.gradle.kts
+++ java/build-logic/build.gradle.kts
@@ -13,5 +13,5 @@ repositories {
 dependencies {
     // This is the only way to pull in a specific version of the 'openrewrite' plugin. Convention plugins like this one
     // can't specify plugin versions in the `plugins {}` DSL block (where you normally would).
-    implementation("org.openrewrite.rewrite:org.openrewrite.rewrite.gradle.plugin:7.35.0")
+    implementation("org.openrewrite.rewrite:org.openrewrite.rewrite.gradle.plugin:7.39.0")
 }
